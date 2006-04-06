7b7afdb6b1c7299fad80f53b3d853d3ad1bba3f6
js163 <js163@80c535d0-b110-0410-b4de-9279f94142c3>
2006-04-06 19:14:08 +0000

    reordering files for creating and dropping the schema, to reflect the fact that screens and screen_results depend on wells (found in libraries)
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@12 80c535d0-b110-0410-b4de-9279f94142c3

--- sql/create_schema.sql
+++ sql/create_schema.sql
@@ -3,7 +3,7 @@
 */
 
 \i create_users.sql
+\i create_libraries.sql
 \i create_screens.sql
 \i create_screen_results.sql
-\i create_libraries.sql
 \i create_derivatives.sql
--- sql/drop_schema.sql
+++ sql/drop_schema.sql
@@ -3,7 +3,7 @@
 */
 
 \i drop_derivatives.sql
-\i drop_libraries.sql
 \i drop_screen_results.sql
 \i drop_screens.sql
+\i drop_libraries.sql
 \i drop_users.sql
