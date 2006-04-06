c48b228b3c0c99c4d802265eeffefafea2783403
js163 <js163@80c535d0-b110-0410-b4de-9279f94142c3>
2006-04-06 19:11:36 +0000

    adding stub table well for referential integrity
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@9 80c535d0-b110-0410-b4de-9279f94142c3

---
+++ sql/create_libraries.sql
@@ -0,0 +1,14 @@
+/* screensaver/sql/create_libraries.sql
+** by john sullivan, 2006.04
+**
+** creates the following tables:
+**   well
+*/
+
+
+/* table well */
+
+/* stub */
+CREATE TABLE well (
+  well_id    SERIAL PRIMARY KEY
+);
---
+++ sql/drop_libraries.sql
@@ -0,0 +1,5 @@
+/* screensaver/sql/drop_libraries.sql
+** by john sullivan, 2006.04
+*/
+
+DROP TABLE well;
