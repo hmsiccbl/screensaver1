ecb9392eaa8ef70875ef832a0fccb476f7bc2ab1
js163 <js163@80c535d0-b110-0410-b4de-9279f94142c3>
2006-05-01 20:21:42 +0000

    moving the original by-hand version of the ddl into a separate directory, so it doesnt get in the way of the hibernate-generated ddl. eventually, we will want to get rid of this altogether, but for the time being, it stands as a nice reference.
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@25 80c535d0-b110-0410-b4de-9279f94142c3

--- sql/create_schema.sql
+++
@@ -1,9 +0,0 @@
-/* screensaver/sql/create_schema.sql
-** by john sullivan, 2006.04
-*/
-
-\i create_users.sql
-\i create_libraries.sql
-\i create_screens.sql
-\i create_screen_results.sql
-\i create_derivatives.sql
--- sql/drop_schema.sql
+++
@@ -1,9 +0,0 @@
-/* screensaver/sql/drop_schema.sql
-** by john sullivan, 2006.04
-*/
-
-\i drop_derivatives.sql
-\i drop_screen_results.sql
-\i drop_screens.sql
-\i drop_libraries.sql
-\i drop_users.sql
