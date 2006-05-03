76b93918a7e3940d5e82aac0670bdd14ddc02095
js163 <js163@80c535d0-b110-0410-b4de-9279f94142c3>
2006-05-03 15:20:52 +0000

    initial version of getting XDoclet to generate .hmb.xml files
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@30 80c535d0-b110-0410-b4de-9279f94142c3

--- src/edu/harvard/med/screensaver/beans/Compound.java
+++ src/edu/harvard/med/screensaver/beans/Compound.java
@@ -5,32 +5,38 @@ package edu.harvard.med.screensaver.beans;
 
 /**
  * @author s
- *
+ * @hibernate.class
+ *   table="COMPOUND"
  */
 public class Compound {
 
-	private Long id;
+	private Long _compoundId;
 	
-	private String smiles;
+	private String _smiles;
 	
 	/**
 	 * @return Returns the id.
+	 * @hibernate.id
+	 *   generator-class="native"
+	 *   column="compound_id"
 	 */
-	public Long getId() {
-		return id;
+	public Long getCompoundId() {
+		return _compoundId;
 	}
 	
 	/**
 	 * @return Returns the smiles.
+	 * @hibernate.property
+	 *   column="smiles"
 	 */
 	public String getSmiles() {
-		return smiles;
+		return _smiles;
 	}
 	
 	/**
 	 * @param smiles The smiles to set.
 	 */
 	public void setSmiles(String smiles) {
-		this.smiles = smiles;
+		_smiles = smiles;
 	}
 }
