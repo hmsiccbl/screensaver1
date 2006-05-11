cc46ab24be537b9eb8a5b5a04988efca72b9b185
js163 <js163@80c535d0-b110-0410-b4de-9279f94142c3>
2006-05-11 19:09:38 +0000

    - genericized the ThreadLocal object - formatted the file more towards project convention - cleaned up the contents of some of the comments
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@80 80c535d0-b110-0410-b4de-9279f94142c3

--- src/edu/harvard/med/screensaver/beans/HibernateSessionFactory.java
+++ src/edu/harvard/med/screensaver/beans/HibernateSessionFactory.java
@@ -7,41 +7,39 @@
 // at Harvard Medical School. This software is distributed under the terms of
 // the GNU General Public License.
 
-
 package edu.harvard.med.screensaver.beans;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.cfg.Configuration;
 
+
 /**
  * Configures and provides access to Hibernate sessions, tied to the
  * current thread of execution.  Follows the Thread Local Session
  * pattern, see {@link http://hibernate.org/42.html}.
  */
-public class HibernateSessionFactory {
-
-  /** 
-   * Location of hibernate.cfg.xml file.
-   * NOTICE: Location should be on the classpath as Hibernate uses
-   * #resourceAsStream style lookup for its configuration file. That
-   * is place the config file in a Java package - the default location
-   * is the default Java package.<br><br>
-   * Examples: <br>
-   * <code>CONFIG_FILE_LOCATION = "/hibernate.conf.xml". 
-   * CONFIG_FILE_LOCATION = "/com/foo/bar/myhiberstuff.conf.xml".</code> 
-   */
+public class HibernateSessionFactory
+{
+  
+  // static fields
+  
+  /** Location of hibernate.cfg.xml file. */
   private static String CONFIG_FILE_LOCATION = "/hibernate.cfg.xml";
 
   /** Holds a single instance of Session */
-  private static final ThreadLocal threadLocal = new ThreadLocal();
+  private static final ThreadLocal<Session> threadLocal =
+    new ThreadLocal<Session>();
 
   /** The single instance of hibernate configuration */
-  private static final Configuration cfg = new Configuration();
+  private static final Configuration configuration = new Configuration();
 
   /** The single instance of hibernate SessionFactory */
   private static org.hibernate.SessionFactory sessionFactory;
 
+  
+  // static methods
+  
   /**
    * Returns the ThreadLocal Session instance.  Lazy initialize
    * the <code>SessionFactory</code> if needed.
@@ -50,13 +48,12 @@ public class HibernateSessionFactory {
    *  @throws HibernateException
    */
   public static Session currentSession() throws HibernateException {
-    Session session = (Session) threadLocal.get();
-
-    if (session == null || !session.isOpen()) {
+    Session session = threadLocal.get();
+    if (session == null || ! session.isOpen()) {
       if (sessionFactory == null) {
         try {
-          cfg.configure(CONFIG_FILE_LOCATION);
-          sessionFactory = cfg.buildSessionFactory();
+          configuration.configure(CONFIG_FILE_LOCATION);
+          sessionFactory = configuration.buildSessionFactory();
         }
         catch (Exception e) {
           System.err.println("%%%% Error Creating SessionFactory %%%%");
@@ -66,28 +63,30 @@ public class HibernateSessionFactory {
       session = (sessionFactory != null) ? sessionFactory.openSession() : null;
       threadLocal.set(session);
     }
-
     return session;
   }
 
   /**
    *  Close the single hibernate session instance.
-   *
    *  @throws HibernateException
    */
   public static void closeSession() throws HibernateException {
     Session session = (Session) threadLocal.get();
     threadLocal.set(null);
-
     if (session != null) {
       session.close();
     }
   }
 
+  
+  // private constructor
+  
   /**
    * Default constructor.
+   * @motivation The default no-arg constructor is private to maintain the
+   *             singleton pattern by preventing others from instantiating
+   *             objects of this type.
    */
   private HibernateSessionFactory() {
   }
-
 }
