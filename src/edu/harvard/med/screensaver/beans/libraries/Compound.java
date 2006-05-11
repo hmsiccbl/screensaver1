d03fbaecb39fbe43af7ead6e4336b23e262ca262
js163 <js163@80c535d0-b110-0410-b4de-9279f94142c3>
2006-05-11 18:55:31 +0000

    adding standard file headers
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@79 80c535d0-b110-0410-b4de-9279f94142c3

--- src/edu/harvard/med/screensaver/beans/libraries/Compound.java
+++ src/edu/harvard/med/screensaver/beans/libraries/Compound.java
@@ -1,11 +1,18 @@
 // $HeadURL$
 // $Id$
+//
+// Copyright © 2006 by the President and Fellows of Harvard College.
+// 
+// Screensaver is an open-source project developed by the ICCB-L and NSRB labs
+// at Harvard Medical School. This software is distributed under the terms of
+// the GNU General Public License.
 
 
 package edu.harvard.med.screensaver.beans.libraries;
 
 import java.util.HashSet;
 import java.util.Set;
+
 
 /**
  * A Hibernate entity bean representing a molecular compound.
