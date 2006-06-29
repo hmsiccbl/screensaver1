437c4256017a0a71046b5cea43f07fec87c55718
js163 <js163@80c535d0-b110-0410-b4de-9279f94142c3>
2006-06-29 19:47:38 +0000

    implemented derivatives portion of the data model
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@286 80c535d0-b110-0410-b4de-9279f94142c3

---
+++ src/edu/harvard/med/screensaver/model/derivatives/Derivative.java
@@ -0,0 +1,318 @@
+// $HeadURL$
+// $Id$
+//
+// Copyright 2006 by the President and Fellows of Harvard College.
+// 
+// Screensaver is an open-source project developed by the ICCB-L and NSRB labs
+// at Harvard Medical School. This software is distributed under the terms of
+// the GNU General Public License.
+
+package edu.harvard.med.screensaver.model.derivatives;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+import edu.harvard.med.screensaver.model.AbstractEntity;
+
+
+/**
+ * A Hibernate entity bean representing a derivative.
+ * 
+ * @author <a mailto="john_sullivan@hms.harvard.edu">John Sullivan</a>
+ * @author <a mailto="andrew_tolopko@hms.harvard.edu">Andrew Tolopko</a>
+ * @hibernate.class lazy="false"
+ */
+public class Derivative extends AbstractEntity
+{
+  
+  // static fields
+
+  private static final Logger log = Logger.getLogger(Derivative.class);
+  private static final long serialVersionUID = 0L;
+
+
+  // instance fields
+
+  private Integer _derivativeId;
+  private Integer _version;
+  private Set<DerivativeScreenResult> _derivativeScreenResults = new HashSet<DerivativeScreenResult>();
+  private String _name;
+  private Set<String> _synonyms = new HashSet<String>();
+  private String _smiles;
+  private String _sdfile;
+
+
+  // public constructor
+
+  /**
+   * Constructs an initialized <code>Derivative</code> object.
+   *
+   * @param name the name
+   * @param smiles the SMILES string
+   */
+  public Derivative(
+    String name,
+    String smiles)
+  {
+    _name = name;
+    _smiles = smiles;
+  }
+
+
+  // public methods
+
+  @Override
+  public Integer getEntityId()
+  {
+    return getDerivativeId();
+  }
+
+  /**
+   * Get the id for the derivative.
+   *
+   * @return the id for the derivative
+   * @hibernate.id generator-class="sequence"
+   * @hibernate.generator-param name="sequence" value="derivative_id_seq"
+   */
+  public Integer getDerivativeId()
+  {
+    return _derivativeId;
+  }
+
+  /**
+   * Get an unmodifiable copy of the set of derivative screen results.
+   *
+   * @return the derivative screen results
+   */
+  public Set<DerivativeScreenResult> getDerivativeScreenResults()
+  {
+    return Collections.unmodifiableSet(_derivativeScreenResults);
+  }
+
+  /**
+   * Add the derivative screen result.
+   *
+   * @param derivativeScreenResult the derivative screen result to add
+   * @return true iff the derivative did not already have the derivative screen result
+   */
+  public boolean addDerivativeScreenResult(DerivativeScreenResult derivativeScreenResult)
+  {
+    if (getHbnDerivativeScreenResults().add(derivativeScreenResult)) {
+      derivativeScreenResult.setHbnDerivative(this);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Get the name.
+   *
+   * @return the name
+   * @hibernate.property
+   *   type="text"
+   *   not-null="true"
+   */
+  public String getName()
+  {
+    return _name;
+  }
+
+  /**
+   * Set the name.
+   *
+   * @param name the new name
+   */
+  public void setName(String name)
+  {
+    _name = name;
+  }
+
+  /**
+   * Get the synonyms.
+   *
+   * @return the synonyms
+   * @hibernate.set
+   *   order-by="synonym"
+   *   table="derivative_synonym"
+   *   cascade="delete"
+   *   lazy="true"
+   * @hibernate.collection-key
+   *   column="derivative_id"
+   *   foreign-key="fk_derivative_synonym_to_derivative"
+   * @hibernate.collection-element
+   *   type="text"
+   *   column="synonym"
+   *   not-null="true"
+   */
+  public Set<String> getSynonyms()
+  {
+    return _synonyms;
+  }
+
+  /**
+   * Add the synonym.
+   *
+   * @param synonym the synonym to add
+   * @return true iff the derivative did not already have the synonym
+   */
+  public boolean addSynonym(String synonym)
+  {
+    return _synonyms.add(synonym);
+  }
+
+  /**
+   * Remove the synonym.
+   *
+   * @param synonym the synonym to remove
+   * @return true iff the derivative previously had the synonym
+   */
+  public boolean removeSynonym(String synonym)
+  {
+    return _synonyms.remove(synonym);
+  }
+
+  /**
+   * Get the SMILES string.
+   *
+   * @return the SMILES string
+   * @hibernate.property
+   *   type="text"
+   *   not-null="true"
+   *   unique="true"
+   */
+  public String getSmiles()
+  {
+    return _smiles;
+  }
+
+  /**
+   * Set the SMILES string.
+   *
+   * @param smiles the new SMILES string
+   */
+  public void setSmiles(String smiles)
+  {
+    _smiles = smiles;
+  }
+
+  /**
+   * Get the SD file.
+   *
+   * @return the SD file
+   * @hibernate.property
+   *   type="text"
+   */
+  public String getSdfile()
+  {
+    return _sdfile;
+  }
+
+  /**
+   * Set the SD file.
+   *
+   * @param sdfile the new SD file
+   */
+  public void setSdfile(String sdfile)
+  {
+    _sdfile = sdfile;
+  }
+
+
+  // protected methods
+
+  @Override
+  protected Object getBusinessKey()
+  {
+    return getSmiles();
+  }
+
+
+  // package methods
+
+  /**
+   * Get the derivative screen results.
+   *
+   * @return the derivative screen results
+   * @hibernate.set
+   *   cascade="save-update"
+   *   inverse="true"
+   * @hibernate.collection-key
+   *   column="derivative_id"
+   * @hibernate.collection-one-to-many
+   *   class="edu.harvard.med.screensaver.model.derivatives.DerivativeScreenResult"
+   * @motivation for hibernate and maintenance of bi-directional relationships
+   */
+  Set<DerivativeScreenResult> getHbnDerivativeScreenResults()
+  {
+    return _derivativeScreenResults;
+  }
+
+
+  // private constructor
+
+  /**
+   * Construct an uninitialized <code>Derivative</code> object.
+   *
+   * @motivation for hibernate
+   */
+  private Derivative() {}
+
+
+  // private methods
+
+  /**
+   * Set the id for the derivative.
+   *
+   * @param derivativeId the new id for the derivative
+   * @motivation for hibernate
+   */
+  private void setDerivativeId(Integer derivativeId) {
+    _derivativeId = derivativeId;
+  }
+
+  /**
+   * Get the version for the derivative.
+   *
+   * @return the version for the derivative
+   * @motivation for hibernate
+   * @hibernate.version
+   */
+  private Integer getVersion() {
+    return _version;
+  }
+
+  /**
+   * Set the version for the derivative.
+   *
+   * @param version the new version for the derivative
+   * @motivation for hibernate
+   */
+  private void setVersion(Integer version) {
+    _version = version;
+  }
+
+  /**
+   * Set the derivative screen results.
+   *
+   * @param derivativeScreenResults the new derivative screen results
+   * @motivation for hibernate
+   */
+  private void setHbnDerivativeScreenResults(Set<DerivativeScreenResult> derivativeScreenResults)
+  {
+    _derivativeScreenResults = derivativeScreenResults;
+  }
+
+  /**
+   * Set the synonyms.
+   *
+   * @param synonyms the new synonyms
+   * @motivation for hibernate
+   */
+  private void setSynonyms(Set<String> synonyms)
+  {
+    _synonyms = synonyms;
+  }
+}
---
+++ src/edu/harvard/med/screensaver/model/derivatives/DerivativeScreenResult.java
@@ -0,0 +1,352 @@
+// $HeadURL$
+// $Id$
+//
+// Copyright 2006 by the President and Fellows of Harvard College.
+// 
+// Screensaver is an open-source project developed by the ICCB-L and NSRB labs
+// at Harvard Medical School. This software is distributed under the terms of
+// the GNU General Public License.
+
+package edu.harvard.med.screensaver.model.derivatives;
+
+
+import org.apache.log4j.Logger;
+
+import edu.harvard.med.screensaver.model.AbstractEntity;
+
+
+/**
+ * A Hibernate entity bean representing a derivative screen result.
+ * 
+ * @author <a mailto="john_sullivan@hms.harvard.edu">John Sullivan</a>
+ * @author <a mailto="andrew_tolopko@hms.harvard.edu">Andrew Tolopko</a>
+ * @hibernate.class lazy="false"
+ */
+public class DerivativeScreenResult extends AbstractEntity
+{
+  
+  // static fields
+
+  private static final Logger log = Logger.getLogger(DerivativeScreenResult.class);
+  private static final long serialVersionUID = 0L;
+
+
+  // instance fields
+
+  private Integer _derivativeScreenResultId;
+  private Integer _version;
+  private Derivative _derivative;
+  private String _activityLevel;
+  private String _activityType;
+
+
+  // public constructor
+
+  /**
+   * Constructs an initialized <code>DerivativeScreenResult</code> object.
+   *
+   * @param derivative the derivative
+   * @param activityLevel the activity level
+   * @param activityType the activity type
+   */
+  public DerivativeScreenResult(
+    Derivative derivative,
+    String activityLevel,
+    String activityType)
+  {
+    _derivative = derivative;
+    _activityLevel = activityLevel;
+    _activityType = activityType;
+    _derivative.getHbnDerivativeScreenResults().add(this);
+  }
+
+
+  // public methods
+
+  @Override
+  public Integer getEntityId()
+  {
+    return getDerivativeScreenResultId();
+  }
+
+  /**
+   * Get the id for the derivative screen result.
+   *
+   * @return the id for the derivative screen result
+   * @hibernate.id generator-class="sequence"
+   * @hibernate.generator-param name="sequence" value="derivative_screen_result_id_seq"
+   */
+  public Integer getDerivativeScreenResultId()
+  {
+    return _derivativeScreenResultId;
+  }
+
+  /**
+   * Get the derivative.
+   *
+   * @return the derivative
+   */
+  public Derivative getDerivative()
+  {
+    return _derivative;
+  }
+
+  /**
+   * Set the derivative.
+   *
+   * @param derivative the new derivative
+   */
+  public void setDerivative(Derivative derivative)
+  {
+    _derivative.getHbnDerivativeScreenResults().remove(this);
+    _derivative = derivative;
+    derivative.getHbnDerivativeScreenResults().add(this);
+  }
+
+  /**
+   * Get the activity level.
+   *
+   * @return the activity level
+   */
+  public String getActivityLevel()
+  {
+    return _activityLevel;
+  }
+
+  /**
+   * Set the activity level.
+   *
+   * @param activityLevel the new activity level
+   */
+  public void setActivityLevel(String activityLevel)
+  {
+    _activityLevel = activityLevel;
+  }
+
+  /**
+   * Get the activity type.
+   *
+   * @return the activity type
+   */
+  public String getActivityType()
+  {
+    return _activityType;
+  }
+
+  /**
+   * Set the activity type.
+   *
+   * @param activityType the new activity type
+   */
+  public void setActivityType(String activityType)
+  {
+    _activityType = activityType;
+  }
+
+
+  // protected methods
+
+  /**
+   * A business key class for the well.
+   */
+  private class BusinessKey
+  {
+
+  /**
+   * Get the derivative.
+   *
+   * @return the derivative
+   */
+  public Derivative getDerivative()
+  {
+    return _derivative;
+  }
+
+  /**
+   * Get the activity level.
+   *
+   * @return the activity level
+   */
+  public String getActivityLevel()
+  {
+    return _activityLevel;
+  }
+
+  /**
+   * Get the activity type.
+   *
+   * @return the activity type
+   */
+  public String getActivityType()
+  {
+    return _activityType;
+  }
+
+    @Override
+    public boolean equals(Object object)
+    {
+      if (! (object instanceof BusinessKey)) {
+        return false;
+      }
+      BusinessKey that = (BusinessKey) object;
+      return
+        getDerivative().equals(that.getDerivative()) &&
+        getActivityLevel().equals(that.getActivityLevel()) &&
+        getActivityType().equals(that.getActivityType());
+    }
+
+    @Override
+    public int hashCode()
+    {
+      return
+        getDerivative().hashCode() +
+        getActivityLevel().hashCode() +
+        getActivityType().hashCode();
+    }
+
+    @Override
+    public String toString()
+    {
+      return getDerivative() + ":" + getActivityLevel() + ":" + getActivityType();
+    }
+  }
+
+  @Override
+  protected Object getBusinessKey()
+  {
+    // TODO: assure changes to business key update relationships whose other side is many
+    return new BusinessKey();
+  }
+
+
+  // package methods
+
+  /**
+   * Set the derivative.
+   * Throw a NullPointerException when the derivative is null.
+   *
+   * @param derivative the new derivative
+   * @throws NullPointerException when the derivative is null
+   * @motivation for hibernate and maintenance of bi-directional relationships
+   */
+  void setHbnDerivative(Derivative derivative)
+  {
+    if (derivative == null) {
+      throw new NullPointerException();
+    }
+    _derivative = derivative;
+  }
+
+
+  // private constructor
+
+  /**
+   * Construct an uninitialized <code>DerivativeScreenResult</code> object.
+   *
+   * @motivation for hibernate
+   */
+  private DerivativeScreenResult() {}
+
+
+  // private methods
+
+  /**
+   * Set the id for the derivative screen result.
+   *
+   * @param derivativeScreenResultId the new id for the derivative screen result
+   * @motivation for hibernate
+   */
+  private void setDerivativeScreenResultId(Integer derivativeScreenResultId) {
+    _derivativeScreenResultId = derivativeScreenResultId;
+  }
+
+  /**
+   * Get the version for the derivative screen result.
+   *
+   * @return the version for the derivative screen result
+   * @motivation for hibernate
+   * @hibernate.version
+   */
+  private Integer getVersion() {
+    return _version;
+  }
+
+  /**
+   * Set the version for the derivative screen result.
+   *
+   * @param version the new version for the derivative screen result
+   * @motivation for hibernate
+   */
+  private void setVersion(Integer version) {
+    _version = version;
+  }
+
+  /**
+   * Get the derivative.
+   *
+   * @return the derivative
+   * @hibernate.many-to-one
+   *   class="edu.harvard.med.screensaver.model.derivatives.Derivative"
+   *   column="derivative_id"
+   *   not-null="true"
+   *   foreign-key="fk_derivative_screen_result_to_derivative"
+   *   cascade="save-update"
+   * @motivation for hibernate
+   */
+  private Derivative getHbnDerivative()
+  {
+    return _derivative;
+  }
+  
+  /**
+   * Get the activity level.
+   *
+   * @return the activity level
+   * @hibernate.property
+   *   column="activity_level"
+   *   type="text"
+   *   not-null="true"
+   * @motivation for hibernate
+   */
+  private String getHbnActivityLevel()
+  {
+    return _activityLevel;
+  }
+
+  /**
+   * Set the activity level.
+   *
+   * @param activityLevel the new activity level
+   * @motivation for hibernate
+   */
+  private void setHbnActivityLevel(String activityLevel)
+  {
+    _activityLevel = activityLevel;
+  }
+
+  /**
+   * Get the activity type.
+   *
+   * @return the activity type
+   * @hibernate.property
+   *   column="activity_type"
+   *   type="text"
+   *   not-null="true"
+   * @motivation for hibernate
+   */
+  private String getHbnActivityType()
+  {
+    return _activityType;
+  }
+
+  /**
+   * Set the activity type.
+   *
+   * @param activityType the new activity type
+   * @motivation for hibernate
+   */
+  private void setHbnActivityType(String activityType)
+  {
+    _activityType = activityType;
+  }
+}
