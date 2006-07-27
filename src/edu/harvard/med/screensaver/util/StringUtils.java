dd842ae86bb49d54c6e2c0c1acf2af9c740ef22c
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2006-07-27 15:17:15 +0000

    Added util.StringUtils, which contains some string formatting methods I've found useful on previous projects (a method similar to Perl's join function)
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@368 80c535d0-b110-0410-b4de-9279f94142c3

---
+++ src/edu/harvard/med/screensaver/util/StringUtils.java
@@ -0,0 +1,76 @@
+
+package edu.harvard.med.screensaver.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class StringUtils {
+
+
+	/**
+   * Makes a delimited list of items from a Collection, just like Perl's join()
+   * function.
+   * 
+   * @param items a <code>Collection</code> of <code>Object</code>s with
+   *          appropriate <code>toString</code> methods
+   * @param delimiter a <code>String</code>
+   * @return a <code>String</code> containing the string representation of the
+   *         Collection elements, delimited by <code>delimiter</code>
+   */
+  /* TODO: provide escaping of specified characters */
+  /* TODO: overload method signature for default parameters */
+  public static String makeListString(Collection items, String delimiter)
+  {
+    boolean isFirst = true;
+    StringBuffer buf = new StringBuffer();
+    for (Iterator iter = items.iterator(); iter.hasNext();) {
+      String item = iter.next()
+                        .toString();
+      if (isFirst) {
+        isFirst = false;
+      }
+      else {
+        buf.append(delimiter);
+      }
+      buf.append(item);
+    }
+    return buf.toString();
+  }
+
+
+	public static String makeRepeatedString(String segment, int count)
+  {
+    int targetLength = segment.length() * count;
+    StringBuffer buf = new StringBuffer(targetLength);
+    for (int i = 0; buf.length() < targetLength; ++i) {
+      // for efficiency, we'll double the size of our repeated string, if
+      // possible
+      if (buf.length() > 0 && buf.length() * 2 <= targetLength) {
+        buf.append(buf);
+      }
+      else {
+        buf.append(segment);
+      }
+    }
+    return buf.toString();
+  }
+
+
+	@SuppressWarnings("unchecked")
+  public static List wrapStrings(List elements, String left, String right)
+  {
+    StringBuffer buf = new StringBuffer();
+    List result = new ArrayList(elements.size());
+    for (Iterator iter = elements.iterator(); iter.hasNext();) {
+      buf.append(left)
+         .append(iter.next())
+         .append(right);
+      result.add(buf.toString());
+      buf.setLength(0);
+    }
+    return result;
+  }
+
+}
