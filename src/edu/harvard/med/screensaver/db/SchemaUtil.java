38c4a80b82d3a27bf6919037f12910c19466ae23
js163 <js163@80c535d0-b110-0410-b4de-9279f94142c3>
2006-08-02 18:49:41 +0000

    despite hibernate complete suckiness in terms of being able to get metadata about my own schema, added a method to truncate all tables. i did it in JDBC/PostgreSQL after hours of trying to do it in hibernate
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@404 80c535d0-b110-0410-b4de-9279f94142c3

--- src/edu/harvard/med/screensaver/db/SchemaUtil.java
+++ src/edu/harvard/med/screensaver/db/SchemaUtil.java
@@ -9,113 +9,37 @@
 
 package edu.harvard.med.screensaver.db;
 
-import edu.harvard.med.screensaver.CommandLineApplication;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.dao.DataAccessException;
 import org.springframework.orm.hibernate3.LocalSessionFactoryBean;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import edu.harvard.med.screensaver.CommandLineApplication;
 
 /**
  * Utility for manipulating schemas, via Spring+Hibernate.
- * @author ant
+ * 
+ * @author <a mailto="john_sullivan@hms.harvard.edu">John Sullivan</a>
+ * @author <a mailto="andrew_tolopko@hms.harvard.edu">Andrew Tolopko</a>
  */
-public class SchemaUtil implements ApplicationContextAware
+public class SchemaUtil extends HibernateDaoSupport implements ApplicationContextAware
 {
+  
+  // static fields
 
   private static Logger log = Logger.getLogger(SchemaUtil.class);
-  
-  
-  /**
-   * The Spring application context.
-   * 
-   * @motivation Needed to get a Hibernate SessionFactory from Spring. Normal
-   *             injection of a SessionFactory is problematic, as Spring appears
-   *             to treat factory beans as special cases, and injects the
-   *             product of a factory, rather than the factory itself.
-   */
-  private ApplicationContext _appCtx;
-  
-  private LocalSessionFactoryBean _sessionFactory;
-
-  private String _sessionFactoryBeanId;
-
-  public void setSessionFactoryBeanId(String sessionFactoryBeanId)
-  {
-    // we go through some hoops to get the sessionFactory bean, because Spring
-    // normally wants to provide a *product* of a factory, when a factory bean
-    // is injected; but we want the factory itself, so we have to use getBean()
-    // with the special "&" prefix syntax. See "3.6.1. Obtaining a FactoryBean,
-    // not its product" in Spring 1.2.x. reference manual.
-    _sessionFactoryBeanId = sessionFactoryBeanId;
-  }
-  
-  /**
-   * Lazy acquisition of LocalSessionFactoryBean.
-   * 
-   * @motivation bean properties are not set in a deterministic order, but we
-   *             depend upon multiple properties.
-   * @return
-   */
-  private LocalSessionFactoryBean getSessionFactory()
-  {
-    if ( _sessionFactory == null ) {
-      _sessionFactory = (LocalSessionFactoryBean) _appCtx.getBean("&" + _sessionFactoryBeanId);
-    }
-    return _sessionFactory;
-  }
-
-  /**
-   * Setter for applicationContext Property.
-   */
-  public void setApplicationContext(ApplicationContext applicationContext)
-  throws BeansException
-  {
-    _appCtx = applicationContext;
-  }
-  
-  /**
-   * Drop the schema that is configured for this Spring+Hibernate enabled project.
-   */
-  public void dropSchema() throws DataAccessException
-  {
-    log.info("dropping schema for " + makeDataSourceString());
-    getSessionFactory().dropDatabaseSchema();
-  }
-  
-  /**
-   * Create the schema that is configured for this Spring+Hibernate enabled project.
-   */
-  public void createSchema() throws DataAccessException
-  {
-    log.info("creating schema for " + makeDataSourceString());
-    getSessionFactory().createDatabaseSchema();
-  }
-  
-  /**
-   * Drop and create (in that order) the schema that is configured for this
-   * Spring+Hibernate enabled project.
-   */
-  public void recreateSchema()
-  {
-    dropSchema();
-    createSchema();
-  }
-
-  
-  // private methods
-  
-  private String makeDataSourceString()
-  {
-    BasicDataSource dataSource = (BasicDataSource) _appCtx.getBean("screensaverDataSource");
-    assert dataSource != null : "spring bean 'screensaverDataSource' not found";
-    return dataSource.getUsername() + "@" + dataSource.getUrl();
-  }
   
   
   // static methods
@@ -161,6 +85,144 @@ public class SchemaUtil implements ApplicationContextAware
     catch (ParseException e) {
       // handled sufficiently by CommandLineApplication
     }
+  }
+
+
+  // instance fields
+  
+  /**
+   * The Spring application context.
+   * 
+   * @motivation Needed to get a Hibernate SessionFactory from Spring. Normal
+   *             injection of a SessionFactory is problematic, as Spring appears
+   *             to treat factory beans as special cases, and injects the
+   *             product of a factory, rather than the factory itself.
+   */
+  private ApplicationContext _appCtx;
+  
+  private LocalSessionFactoryBean _sessionFactory;
+
+  private String _sessionFactoryBeanId;
+
+  
+  // instance methods
+  
+  public void setSessionFactoryBeanId(String sessionFactoryBeanId)
+  {
+    // we go through some hoops to get the sessionFactory bean, because Spring
+    // normally wants to provide a *product* of a factory, when a factory bean
+    // is injected; but we want the factory itself, so we have to use getBean()
+    // with the special "&" prefix syntax. See "3.6.1. Obtaining a FactoryBean,
+    // not its product" in Spring 1.2.x. reference manual.
+    _sessionFactoryBeanId = sessionFactoryBeanId;
+  }
+  
+  /**
+   * Setter for applicationContext Property.
+   */
+  public void setApplicationContext(ApplicationContext applicationContext)
+  throws BeansException
+  {
+    _appCtx = applicationContext;
+  }
+  
+  /**
+   * Drop the schema that is configured for this Spring+Hibernate enabled project.
+   */
+  public void dropSchema() throws DataAccessException
+  {
+    log.info("dropping schema for " + makeDataSourceString());
+    getLocalSessionFactoryBean().dropDatabaseSchema();
+  }
+  
+  /**
+   * Create the schema that is configured for this Spring+Hibernate enabled project.
+   */
+  public void createSchema() throws DataAccessException
+  {
+    log.info("creating schema for " + makeDataSourceString());
+    getLocalSessionFactoryBean().createDatabaseSchema();
+  }
+  
+  /**
+   * Drop and create (in that order) the schema that is configured for this
+   * Spring+Hibernate enabled project.
+   */
+  public void recreateSchema() throws DataAccessException
+  {
+    dropSchema();
+    createSchema();
+  }
+  
+  @SuppressWarnings("unchecked")
+  public void truncateTables()
+  {
+    log.info("truncating tables for " + makeDataSourceString());
+    Connection connection = getSession().connection();
+    
+    try {
+      String url = connection.getMetaData().getURL();
+      String schemaName = url.substring(url.lastIndexOf('/') + 1);
+
+      Statement statement = connection.createStatement();
+      statement.execute(
+        "SELECT table_name FROM information_schema.tables\n" +
+        "WHERE\n" +
+        " table_catalog = '" + schemaName + "' AND\n" +
+        " table_schema = 'public'\n");
+      
+      String sql = "TRUNCATE TABLE "; 
+      ResultSet resultSet = statement.getResultSet();
+      while (resultSet.next()) {
+        sql += resultSet.getString(1) + ", ";
+      }
+      
+      // in case there are no tables in the schema
+      if (sql.equals("TRUNCATE TABLE ")) {
+        return;
+      }
+      
+      sql = sql.substring(0, sql.length() - 2);
+      statement.close();
+
+      statement = connection.createStatement();
+      statement.execute(sql);
+      statement.close();
+    }
+    catch (HibernateException e) {
+      throw convertHibernateAccessException(e);
+    }
+    catch (IllegalStateException e) {
+      log.error("bad illegal state exception", e);
+    }
+    catch (SQLException e) {
+      log.error("bad sql exception", e);
+    }
+  }
+
+  
+  // private methods
+  
+  /**
+   * Lazy acquisition of LocalSessionFactoryBean.
+   * 
+   * @motivation bean properties are not set in a deterministic order, but we
+   *             depend upon multiple properties.
+   * @return
+   */
+  private LocalSessionFactoryBean getLocalSessionFactoryBean()
+  {
+    if ( _sessionFactory == null ) {
+      _sessionFactory = (LocalSessionFactoryBean) _appCtx.getBean("&" + _sessionFactoryBeanId);
+    }
+    return _sessionFactory;
+  }
+
+  private String makeDataSourceString()
+  {
+    BasicDataSource dataSource = (BasicDataSource) _appCtx.getBean("screensaverDataSource");
+    assert dataSource != null : "spring bean 'screensaverDataSource' not found";
+    return dataSource.getUsername() + "@" + dataSource.getUrl();
   }
 
 }
