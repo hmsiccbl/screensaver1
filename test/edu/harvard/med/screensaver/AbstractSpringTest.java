f6872a03fea4c6b6fe89c77499fc679111375add
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2006-08-16 19:59:08 +0000

    AbstractSpringTest now configures the logger with our debug settings (using the log4j.debug.properties file).
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@458 80c535d0-b110-0410-b4de-9279f94142c3

--- test/edu/harvard/med/screensaver/AbstractSpringTest.java
+++ test/edu/harvard/med/screensaver/AbstractSpringTest.java
@@ -1,11 +1,11 @@
-// $HeadURL$
-// $Id$
+//$HeadURL$
+//$Id$
 //
-// Copyright 2006 by the President and Fellows of Harvard College.
-// 
-// Screensaver is an open-source project developed by the ICCB-L and NSRB labs
-// at Harvard Medical School. This software is distributed under the terms of
-// the GNU General Public License.
+//Copyright 2006 by the President and Fellows of Harvard College.
+//
+//Screensaver is an open-source project developed by the ICCB-L and NSRB labs
+//at Harvard Medical School. This software is distributed under the terms of
+//the GNU General Public License.
 
 package edu.harvard.med.screensaver;
 
@@ -21,30 +21,41 @@ import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
  * @author <a mailto="andrew_tolopko@hms.harvard.edu">Andrew Tolopko</a>
  */
 public abstract class AbstractSpringTest
-  extends AbstractDependencyInjectionSpringContextTests
+extends AbstractDependencyInjectionSpringContextTests
 {
+  protected LogConfigurer logConfigurer;
   
   /**
    * Spring configuration will be loaded from the configuration file(s)
    * specified in this constant.
    */
-   private static final String[] SPRING_CONFIG_FILES = new String[] {
-     "spring-context.xml",
-   };
-   
-   
-   public AbstractSpringTest() {
-     // have AbstractDependencyInjectionSpringContextTests inject the properties
-     // we need into protected data members that share the same name as beans in
-     // our Spring configuration files.
-     setPopulateProtectedVariables(true);
-   }
-
-   /* (non-Javadoc)
-    * @see org.springframework.test.AbstractDependencyInjectionSpringContextTests#getConfigLocations()
-    */
-   @Override
-   protected String[] getConfigLocations() {
-     return SPRING_CONFIG_FILES;
-   }
+  private static final String[] SPRING_CONFIG_FILES = new String[] {
+    "spring-context.xml",
+  };
+  
+  
+  public AbstractSpringTest() {
+    // have AbstractDependencyInjectionSpringContextTests inject the properties
+    // we need into protected data members that share the same name as beans in
+    // our Spring configuration files.
+    setPopulateProtectedVariables(true);
+  }
+  
+  /* (non-Javadoc)
+   * @see org.springframework.test.AbstractDependencyInjectionSpringContextTests#getConfigLocations()
+   */
+  @Override
+  protected String[] getConfigLocations() {
+    return SPRING_CONFIG_FILES;
+  }
+  
+  @Override
+  /**
+   * <i>If overriding this method in a subclass, remember to call <code>super.onSetUp()</code>!</i>
+   */
+  protected void onSetUp() throws Exception
+  {
+    // setup debug-level logging
+    logConfigurer.setLogPropertiesResource("classpath:log4j.debug.properties");
+  }
 }
