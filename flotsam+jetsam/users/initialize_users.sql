893ae6ddbbb6f9b49f1ab631a46974aa4b929fad
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2006-09-01 16:43:44 +0000

    added an eCommons user to initialize_users.sql script
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@476 80c535d0-b110-0410-b4de-9279f94142c3

--- flotsam+jetsam/users/initialize_users.sql
+++ flotsam+jetsam/users/initialize_users.sql
@@ -1,6 +1,12 @@
 delete from role_user_link;
 delete from screensaver_user_role;
 delete from screensaver_user;
-insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id) values (1, 1, 'today', 'first', 'last', 'first_last@hms.harvard.edu', 'test');
+
 insert into screensaver_user_role (screensaver_user_role_id, version, role_name) values (1, 1, 'user');
+insert into screensaver_user_role (screensaver_user_role_id, version, role_name) values (2, 1, 'developer');
+
+insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id) values (1, 1, 'today', 'first', 'last', 'first_last@hms.harvard.edu', 'test');
 insert into role_user_link (screensaver_user_role_id, screensaver_user_id) values (1,1);
+
+insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id) values (2, 1, 'today', 'Andrew', 'Tolopko', 'andrew_tolopko@hms.harvard.edu', 'ant4');
+insert into role_user_link (screensaver_user_role_id, screensaver_user_id) values (2,2);
