62c1acbae008a30eb4103fede6ccd26f2667f2e5
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2006-10-03 02:27:27 +0000

    Developers have been promoted!  They now have screensAdmin role.
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@612 80c535d0-b110-0410-b4de-9279f94142c3

--- resources/sql/initialize_database/01_initialize_users.sql
+++ resources/sql/initialize_database/01_initialize_users.sql
@@ -23,6 +23,7 @@ insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_ro
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'readEverythingAdmin';
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'usersAdmin';
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'librariesAdmin';
+insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screensAdmin';
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screenResultsAdmin';
 
 insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id, digested_password, ecommons_id) values (nextval('screensaver_user_id_seq'), 1, 'today', 'john', 'sullivan', 'john_sullivan@hms.harvard.edu', 's', '102efc1a35e83cc91266bc8c6d34ea50c8eef0bc', 'js163');
@@ -31,6 +32,7 @@ insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_ro
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'readEverythingAdmin';
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'usersAdmin';
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'librariesAdmin';
+insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screensAdmin';
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screenResultsAdmin';
 
 
