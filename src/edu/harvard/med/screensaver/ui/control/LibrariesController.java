926fbc1aa7b995a6b44def0eca0982cd1134b11c
js163 <js163@80c535d0-b110-0410-b4de-9279f94142c3>
2006-11-01 00:19:14 +0000

    stub controller classes
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@715 80c535d0-b110-0410-b4de-9279f94142c3

---
+++ src/edu/harvard/med/screensaver/ui/control/LibrariesController.java
@@ -0,0 +1,28 @@
+// $HeadURL: svn+ssh://js163@orchestra.med.harvard.edu/svn/iccb/screensaver/trunk/.eclipse.prefs/codetemplates.xml $
+// $Id: codetemplates.xml 169 2006-06-14 21:57:49Z js163 $
+//
+// Copyright 2006 by the President and Fellows of Harvard College.
+// 
+// Screensaver is an open-source project developed by the ICCB-L and NSRB labs
+// at Harvard Medical School. This software is distributed under the terms of
+// the GNU General Public License.
+
+package edu.harvard.med.screensaver.ui.control;
+
+import org.apache.log4j.Logger;
+
+import edu.harvard.med.screensaver.ui.AbstractBackingBean;
+
+/**
+ * 
+ *
+ * @author <a mailto="john_sullivan@hms.harvard.edu">John Sullivan</a>
+ * @author <a mailto="andrew_tolopko@hms.harvard.edu">Andrew Tolopko</a>
+ */
+public class LibrariesController extends AbstractBackingBean
+{
+  private static Logger log = Logger.getLogger(LibrariesController.class);
+
+
+}
+
