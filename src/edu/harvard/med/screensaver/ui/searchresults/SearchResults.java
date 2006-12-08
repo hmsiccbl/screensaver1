52a8fa6ecc0bdac3183db1a36a9f396b07436be5
js163 <js163@80c535d0-b110-0410-b4de-9279f94142c3>
2006-12-08 16:45:33 +0000

    added an "All" option to the items per page selector
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@821 80c535d0-b110-0410-b4de-9279f94142c3

--- src/edu/harvard/med/screensaver/ui/searchresults/SearchResults.java
+++ src/edu/harvard/med/screensaver/ui/searchresults/SearchResults.java
@@ -62,7 +62,8 @@ abstract public class SearchResults<E extends AbstractEntity> extends AbstractBa
   private static final String EXCEL_FILE = "Excel Spreadsheet";
   private static final String [] DOWNLOAD_FORMATS = { "", EXCEL_FILE, SD_FILE };
 
-  private static final List<Integer> PAGE_SIZE_SELECTIONS = Arrays.asList(10, 20, 50, 100);
+  private static final List<String> PAGE_SIZE_SELECTIONS =
+    Arrays.asList("10", "20", "50", "100", "All");
   
 
   // private instance data
@@ -76,7 +77,7 @@ abstract public class SearchResults<E extends AbstractEntity> extends AbstractBa
   private int _resultsSize;
   private int _currentPageIndex = 0;
   private int _currentEntityIndex = 0;
-  private UISelectOneBean<Integer> _itemsPerPage = new UISelectOneBean<Integer>(PAGE_SIZE_SELECTIONS);
+  private UISelectOneBean<String> _itemsPerPage = new UISelectOneBean<String>(PAGE_SIZE_SELECTIONS);
   private String _downloadFormat = "";
   
   private UIData _dataTable;
@@ -208,7 +209,7 @@ abstract public class SearchResults<E extends AbstractEntity> extends AbstractBa
     if (_resultsSize == 0) {    // special case if results are empty
       return 0;
     }
-    return (_currentPageIndex * _itemsPerPage.getSelection()) + 1;
+    return (_currentPageIndex * getNumItemsPerPage()) + 1;
   }
   
   /**
@@ -217,7 +218,7 @@ abstract public class SearchResults<E extends AbstractEntity> extends AbstractBa
    */
   public int getLastIndex()
   {
-    int lastIndex = (_currentPageIndex + 1) * getItemsPerPage().getSelection();
+    int lastIndex = (_currentPageIndex + 1) * getNumItemsPerPage();
     if (lastIndex > _resultsSize) {
       lastIndex = _resultsSize;
     }
@@ -249,11 +250,20 @@ abstract public class SearchResults<E extends AbstractEntity> extends AbstractBa
    * Get the number of items currently being displayed on a page.
    * @return the number of items currently being displayed on a page
    */
-  public UISelectOneBean<Integer> getItemsPerPage()
+  public UISelectOneBean<String> getItemsPerPageSelector()
   {
     return _itemsPerPage;
   }
   
+  public int getNumItemsPerPage()
+  {
+    String itemsPerPage = _itemsPerPage.getSelection();
+    if (itemsPerPage.equals("All")) {
+      return _resultsSize;
+    }
+    return new Integer(itemsPerPage);
+  }
+
   /**
    * Get the current sort column name.
    * 
@@ -433,7 +443,7 @@ abstract public class SearchResults<E extends AbstractEntity> extends AbstractBa
   {
     if (getViewMode().equals(SearchResultsViewMode.SUMMARY)) {
       _currentPageIndex = Math.min(_currentPageIndex + 1,
-                                   Math.max(0, _resultsSize - 1) / getItemsPerPage().getSelection());
+                                   Math.max(0, _resultsSize - 1) / getNumItemsPerPage());
     }
     else {
       _currentEntityIndex = Math.min(_currentEntityIndex + 1,
@@ -451,7 +461,7 @@ abstract public class SearchResults<E extends AbstractEntity> extends AbstractBa
   public String lastPage()
   {
     if (getViewMode().equals(SearchResultsViewMode.SUMMARY)) {
-      _currentPageIndex = Math.max(0, _resultsSize - 1) / getItemsPerPage().getSelection();
+      _currentPageIndex = Math.max(0, _resultsSize - 1) / getNumItemsPerPage();
     }
     else {
       _currentEntityIndex = _resultsSize - 1;
@@ -494,7 +504,7 @@ abstract public class SearchResults<E extends AbstractEntity> extends AbstractBa
    */
   public String updateItemsPerPage()
   {
-    getDataTable().setRows(getItemsPerPage().getSelection());
+    getDataTable().setRows(getNumItemsPerPage());
     getDataTable().setFirst(0);
     _currentPageIndex = 0;
     return REDISPLAY_PAGE_ACTION_RESULT;
@@ -694,7 +704,7 @@ abstract public class SearchResults<E extends AbstractEntity> extends AbstractBa
   {
     if (getViewMode().equals(SearchResultsViewMode.SUMMARY)) {
       // update the search results summary table
-      getDataTable().setFirst(_currentPageIndex * getItemsPerPage().getSelection());
+      getDataTable().setFirst(_currentPageIndex * getNumItemsPerPage());
     }
     else {
       // update the entity viewer
