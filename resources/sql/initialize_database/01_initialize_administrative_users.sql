2428348ae810d1dd729d3bd8c68abfe76dd06a1d
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-03-22 12:48:30 +0000

    changed admin account for Nathan Moerke to have non-ecommons login; ecommons was used by his earlier screener user account
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1189 80c535d0-b110-0410-b4de-9279f94142c3

--- resources/sql/initialize_database/01_initialize_administrative_users.sql
+++ resources/sql/initialize_database/01_initialize_administrative_users.sql
@@ -105,7 +105,7 @@ insert into screensaver_user (screensaver_user_id, version, date_created, first_
 insert into administrator_user (screensaver_user_id) values (currval('screensaver_user_id_seq'));
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'readEverythingAdmin';
 
-insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id, digested_password, ecommons_id) values (nextval('screensaver_user_id_seq'), 1, 'today', 'Nathan', 'Moerke', 'nathan_moerke@hms.harvard.edu', null, null, 'njm2');
+insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id, digested_password, ecommons_id) values (nextval('screensaver_user_id_seq'), 1, 'today', 'Nathan', 'Moerke', 'nathan_moerke@hms.harvard.edu', 'nmoerke', 'd805a86e89cadb3f27357f75257ae737c7b800cc', null);
 insert into administrator_user (screensaver_user_id) values (currval('screensaver_user_id_seq'));
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'readEverythingAdmin';
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screensAdmin';
