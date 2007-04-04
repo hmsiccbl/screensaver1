fe662a570746fc6095fa89b6c1a615b919922445
s <s@80c535d0-b110-0410-b4de-9279f94142c3>
2007-04-04 21:09:11 +0000

    fixed typo in djw11 ecommons id
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1222 80c535d0-b110-0410-b4de-9279f94142c3

--- resources/sql/initialize_database/01_initialize_administrative_users.sql
+++ resources/sql/initialize_database/01_initialize_administrative_users.sql
@@ -52,7 +52,7 @@ insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_ro
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'usersAdmin';
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screensAdmin';
 
-insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id, digested_password, ecommons_id) values (nextval('screensaver_user_id_seq'), 1, 'today', 'David', 'Wrobel', 'david_wrobel@hms.harvard.edu', 'dwrobel', '491162cda9054e8b010fef7b19996fda6c2aaa3d', 'dhw11');
+insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id, digested_password, ecommons_id) values (nextval('screensaver_user_id_seq'), 1, 'today', 'David', 'Wrobel', 'david_wrobel@hms.harvard.edu', 'dwrobel', '491162cda9054e8b010fef7b19996fda6c2aaa3d', 'djw11');
 insert into administrator_user (screensaver_user_id) values (currval('screensaver_user_id_seq'));
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'readEverythingAdmin';
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'librariesAdmin';
