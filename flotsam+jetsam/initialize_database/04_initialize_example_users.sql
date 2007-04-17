1a1624ed53674837e6c3bfa8c66b3baa6a04b7d9
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-04-17 22:08:57 +0000

    CherryPickAdmin role for test admin account
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1276 80c535d0-b110-0410-b4de-9279f94142c3

--- flotsam+jetsam/initialize_database/04_initialize_example_users.sql
+++ flotsam+jetsam/initialize_database/04_initialize_example_users.sql
@@ -1,3 +1,5 @@
+begin;
+
 /* password=scr33n3r */
 insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id, digested_password) values (nextval('screensaver_user_id_seq'), 1, 'today', 'Compound', 'Screener', 'cscreener@hms.harvard.edu', 'cscreener', '4282a78386b751f631f6caa61a05ca6b0d3eda75'); 
 insert into screening_room_user (screensaver_user_id, user_classification, non_screening_user) values (currval('screensaver_user_id_seq'), 'ICCB-L/NSRB staff', false);
@@ -14,3 +16,6 @@ insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_ro
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'librariesAdmin';
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screensAdmin';
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screenResultsAdmin';
+insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'cherryPickAdmin';
+
+commit;
