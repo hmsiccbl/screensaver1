dc298f01a71c9df6d4efdb2e71f6d8b67f15ff24
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-05-01 13:36:28 +0000

    fixed 1336.sql syntax error
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1342 80c535d0-b110-0410-b4de-9279f94142c3

--- flotsam+jetsam/manual_schema_updates/1336.sql
+++ flotsam+jetsam/manual_schema_updates/1336.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 UPDATE screensaver_revision SET screensaver_revision=1336;
 
-# I think this temporary index does help speed the following queries; it does get used, according to EXPLAIN...
+/* I think this temporary index does help speed the following queries; it does get used, according to EXPLAIN... */
 CREATE INDEX activity_indicator_type_index ON result_value_type (activity_indicator_type);
 
 UPDATE result_value_type_result_values set value = '1' from result_value_type rvt where rvt.result_value_type_id = result_value_type_result_values.result_value_type_id and rvt.activity_indicator_type = 'Partition' and value = 'W';
