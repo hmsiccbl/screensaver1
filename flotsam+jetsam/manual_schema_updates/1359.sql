7c0e6d94809007ea80f06ea50f7f82088ce16dce
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-05-18 19:31:45 +0000

    fixed 1359.sql
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1361 80c535d0-b110-0410-b4de-9279f94142c3

--- flotsam+jetsam/manual_schema_updates/1359.sql
+++ flotsam+jetsam/manual_schema_updates/1359.sql
@@ -6,7 +6,7 @@ ALTER TABLE cherry_pick_assay_plate ADD COLUMN legacy_plate_name text;
 ALTER TABLE cherry_pick_assay_plate ADD COLUMN is_legacy bool;
 UPDATE cherry_pick_assay_plate set is_legacy = 't', legacy_plate_name = substr(comments, 44, length(comments) - 45) where strpos(comments, 'assay plate is') > 0;
 UPDATE cherry_pick_assay_plate set is_legacy = 'f' where is_legacy is null;
-ALTER TABLE cherry_pick_assay_plate ALTER COLUMN is_legacy bool SET NOT NULL;
+ALTER TABLE cherry_pick_assay_plate ALTER COLUMN is_legacy SET NOT NULL;
 ALTER TABLE cherry_pick_assay_plate DROP COLUMN comments;
 
 COMMIT;
