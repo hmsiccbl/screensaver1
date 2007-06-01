d3362504ff0901fe7f727515ab9091d1a3846c8d
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-06-01 20:57:44 +0000

    fixed 1380.sql, which destroyed primary keys on 3 tables.  ouch!  caught this since lack of primary key was causing major performance problems.
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1388 80c535d0-b110-0410-b4de-9279f94142c3

--- flotsam+jetsam/manual_schema_updates/1380.sql
+++ flotsam+jetsam/manual_schema_updates/1380.sql
@@ -19,18 +19,21 @@ UPDATE gene_old_entrezgene_id SET gene_id_2 = cast(gene_id AS integer);
 ALTER TABLE gene_old_entrezgene_id DROP COLUMN gene_id;
 ALTER TABLE gene_old_entrezgene_id RENAME COLUMN gene_id_2 TO gene_id;
 ALTER TABLE gene_old_entrezgene_id ALTER COLUMN gene_id SET NOT NULL;
+ALTER TABLE gene_old_entrezgene_id ADD CONSTRAINT gene_old_entrezgene_id_pkey PRIMARY KEY (gene_id, old_entrezgene_id);
 
 ALTER TABLE gene_old_entrezgene_symbol ADD COLUMN gene_id_2 integer;
 UPDATE gene_old_entrezgene_symbol SET gene_id_2 = cast(gene_id AS integer);
 ALTER TABLE gene_old_entrezgene_symbol DROP COLUMN gene_id;
 ALTER TABLE gene_old_entrezgene_symbol RENAME COLUMN gene_id_2 TO gene_id;
 ALTER TABLE gene_old_entrezgene_symbol ALTER COLUMN gene_id SET NOT NULL;
+ALTER TABLE gene_old_entrezgene_symbol ADD CONSTRAINT gene_old_entrezgene_symbol_pkey PRIMARY KEY (gene_id, old_entrezgene_symbol);
 
 ALTER TABLE gene_genbank_accession_number ADD COLUMN gene_id_2 integer;
 UPDATE gene_genbank_accession_number SET gene_id_2 = cast(gene_id AS integer);
 ALTER TABLE gene_genbank_accession_number DROP COLUMN gene_id;
 ALTER TABLE gene_genbank_accession_number RENAME COLUMN gene_id_2 TO gene_id;
 ALTER TABLE gene_genbank_accession_number ALTER COLUMN gene_id SET NOT NULL;
+ALTER TABLE gene_genbank_accession_number ADD CONSTRAINT gene_genbank_accession_number_pkey PRIMARY KEY (gene_id, genbank_accession_number);
 
 alter table silencing_reagent 
     add constraint fk_silencing_reagent_to_gene 
