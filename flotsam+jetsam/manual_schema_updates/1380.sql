15b5cf0d7267f896c35e4d3433dea8029656e825
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-06-05 14:51:41 +0000

    corrected 1380.sql for postgres 7.x
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1393 80c535d0-b110-0410-b4de-9279f94142c3

--- flotsam+jetsam/manual_schema_updates/1380.sql
+++ flotsam+jetsam/manual_schema_updates/1380.sql
@@ -3,33 +3,33 @@ BEGIN;
 UPDATE screensaver_revision SET screensaver_revision=1380;
  
 ALTER TABLE gene ADD COLUMN gene_id_2 integer;
-UPDATE gene SET gene_id_2 = cast(gene_id AS integer);
+UPDATE gene SET gene_id_2 = int4(gene_id);
 ALTER TABLE gene DROP COLUMN gene_id CASCADE;
 ALTER TABLE gene RENAME COLUMN gene_id_2 TO gene_id;
 ALTER TABLE gene ADD PRIMARY KEY (gene_id);
 
 ALTER TABLE silencing_reagent ADD COLUMN gene_id_2 integer;
-UPDATE silencing_reagent SET gene_id_2 = cast(gene_id AS integer);
+UPDATE silencing_reagent SET gene_id_2 = int4(gene_id);
 ALTER TABLE silencing_reagent DROP COLUMN gene_id;
 ALTER TABLE silencing_reagent RENAME COLUMN gene_id_2 TO gene_id;
 ALTER TABLE silencing_reagent ALTER COLUMN gene_id SET NOT NULL;
 
 ALTER TABLE gene_old_entrezgene_id ADD COLUMN gene_id_2 integer;
-UPDATE gene_old_entrezgene_id SET gene_id_2 = cast(gene_id AS integer);
+UPDATE gene_old_entrezgene_id SET gene_id_2 = int4(gene_id);
 ALTER TABLE gene_old_entrezgene_id DROP COLUMN gene_id;
 ALTER TABLE gene_old_entrezgene_id RENAME COLUMN gene_id_2 TO gene_id;
 ALTER TABLE gene_old_entrezgene_id ALTER COLUMN gene_id SET NOT NULL;
 ALTER TABLE gene_old_entrezgene_id ADD CONSTRAINT gene_old_entrezgene_id_pkey PRIMARY KEY (gene_id, old_entrezgene_id);
 
 ALTER TABLE gene_old_entrezgene_symbol ADD COLUMN gene_id_2 integer;
-UPDATE gene_old_entrezgene_symbol SET gene_id_2 = cast(gene_id AS integer);
+UPDATE gene_old_entrezgene_symbol SET gene_id_2 = int4(gene_id);
 ALTER TABLE gene_old_entrezgene_symbol DROP COLUMN gene_id;
 ALTER TABLE gene_old_entrezgene_symbol RENAME COLUMN gene_id_2 TO gene_id;
 ALTER TABLE gene_old_entrezgene_symbol ALTER COLUMN gene_id SET NOT NULL;
 ALTER TABLE gene_old_entrezgene_symbol ADD CONSTRAINT gene_old_entrezgene_symbol_pkey PRIMARY KEY (gene_id, old_entrezgene_symbol);
 
 ALTER TABLE gene_genbank_accession_number ADD COLUMN gene_id_2 integer;
-UPDATE gene_genbank_accession_number SET gene_id_2 = cast(gene_id AS integer);
+UPDATE gene_genbank_accession_number SET gene_id_2 = int4(gene_id);
 ALTER TABLE gene_genbank_accession_number DROP COLUMN gene_id;
 ALTER TABLE gene_genbank_accession_number RENAME COLUMN gene_id_2 TO gene_id;
 ALTER TABLE gene_genbank_accession_number ALTER COLUMN gene_id SET NOT NULL;
