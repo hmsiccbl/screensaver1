bb803fcd52ebc2aeba12877e67b92ac32f1fe0e1
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-06-22 13:17:12 +0000

    Resolved #82230 (screen viewer data table only show 1 data page for screen 518).  Caused by Hibernate bug! (And it was resolved w/o touching any code under screensaver/src!)
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1477 80c535d0-b110-0410-b4de-9279f94142c3

--- test/edu/harvard/med/screensaver/model/screenresults/ResultValueTypeTest.java
+++ test/edu/harvard/med/screensaver/model/screenresults/ResultValueTypeTest.java
@@ -10,8 +10,18 @@
 package edu.harvard.med.screensaver.model.screenresults;
 
 import java.beans.IntrospectionException;
+import java.io.File;
 
+import edu.harvard.med.screensaver.db.DAOTransaction;
+import edu.harvard.med.screensaver.db.LibrariesDAO;
+import edu.harvard.med.screensaver.io.screenresults.ScreenResultParser;
+import edu.harvard.med.screensaver.io.screenresults.ScreenResultParserTest;
 import edu.harvard.med.screensaver.model.AbstractEntityInstanceTest;
+import edu.harvard.med.screensaver.model.MakeDummyEntities;
+import edu.harvard.med.screensaver.model.libraries.Library;
+import edu.harvard.med.screensaver.model.libraries.LibraryType;
+import edu.harvard.med.screensaver.model.screens.Screen;
+import edu.harvard.med.screensaver.model.screens.ScreenType;
 
 import org.apache.log4j.Logger;
 
@@ -21,15 +31,59 @@ public class ResultValueTypeTest extends AbstractEntityInstanceTest
 
   private static Logger log = Logger.getLogger(ResultValueTypeTest.class);
 
+  public static final String SCREEN_RESULT_FILE = "ScreenResultTest115-result-values-size.xls";
+
 
   // instance data members
 
+  protected ScreenResultParser screenResultParser;
+  protected LibrariesDAO librariesDao;
+  
   
   // public constructors and methods
 
   public ResultValueTypeTest() throws IntrospectionException
   {
     super(ResultValueType.class);
+  }
+  
+  /**
+   * Regression test for (apparent) Hibernate bug, which determines size of
+   * extra-lazy value-typed collections by counting non-null values of the first
+   * declared property, which may be nullable. If nullable, the reported
+   * collection size will be less than the real collection size!  RT #82230.
+   */
+  public void testResultValuesSize()
+  {
+    schemaUtil.truncateTablesOrCreateSchema();
+    Library library = new Library("name", "short", ScreenType.SMALL_MOLECULE, LibraryType.DOS, 1, 3);
+    genericEntityDao.persistEntity(library);
+    librariesDao.loadOrCreateWellsForLibrary(library);
+
+    genericEntityDao.doInTransaction(new DAOTransaction() 
+    {
+      public void runTransaction()
+      {
+        File workbookFile = new File(ScreenResultParserTest.TEST_INPUT_FILE_DIR, SCREEN_RESULT_FILE);
+        final Screen screen = MakeDummyEntities.makeDummyScreen(115);
+        screenResultParser.parse(screen, workbookFile);
+        log.debug(screenResultParser.getErrors());
+        assertFalse("screen result had no errors", screenResultParser.getHasErrors());
+        genericEntityDao.persistEntity(screen);
+        
+      }
+    });
+    
+    genericEntityDao.doInTransaction(new DAOTransaction() 
+    {
+      public void runTransaction()
+      {
+        Screen screen = genericEntityDao.findEntityByProperty(Screen.class, "hbnScreenNumber", 115); 
+        assertEquals("result values count", 
+                     28,
+                     screen.getScreenResult().getResultValueTypesList().get(0).getResultValues().size());
+      }
+    });
   }
 }
 
