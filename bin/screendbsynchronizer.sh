695b5f52db581f5173dbf7a3565a1abc0a20aa0d
js163 <js163@80c535d0-b110-0410-b4de-9279f94142c3>
2007-06-26 15:59:08 +0000

    command-line version of the screendb synchronizer - intended to be run on orchestra
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1497 80c535d0-b110-0410-b4de-9279f94142c3

---
+++ bin/screendbsynchronizer.sh
@@ -0,0 +1,24 @@
+# run bin/orchestra/ss-build first!
+#
+# run this proggie something like this:
+#
+# bsub /home/js163/screensaver/bin/screendbsynchronizer.sh
+#
+# TODO: improve placement of output and error files:
+#   - some place secure?
+#   - put a timestamp or something in the filenames so they dont overwrite
+#   - check for existence of files before overwriting?
+
+SCREENSAVER_CHECKOUT=$HOME/screensaver
+BUILD=$SCREENSAVER_CHECKOUT/build
+
+cd $SCREENSAVER_CHECKOUT
+
+LIBS=`for s in $BUILD/lib/*.jar ; do printf ":$s" ; done`
+CLASSPATH="$BUILD/classes$LIBS"
+
+java -Xmx1500m -cp $CLASSPATH edu.harvard.med.screensaver.db.screendb.OrchestraStandaloneScreenDBSynchronizer -S pgsql.cl.med.harvard.edu -D screendb -U $USER > screendbsynchronizer.out 2> screendbsynchronizer.err
+
+echo screendbsynchronizer.sh is complete
+echo program output is in $SCREENSAVER_CHECKOUT/screendbsynchronizer.{out,err}
+
