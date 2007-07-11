f3ca89a1a768fdc410461873e0e5c7d1e631e0ea
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-07-11 17:46:53 +0000

    added bin/import-screen-result.sh, allowing screen results to be imported on orchestra via the LSF cluster
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1538 80c535d0-b110-0410-b4de-9279f94142c3

---
+++ bin/import-screen-result.sh
@@ -0,0 +1,26 @@
+# run bin/orchestra/ss-build first!
+#
+# usage:
+# bsub ~/screensaver/bin/import-screen-result.sh <screen#>
+#
+# usage (interactive/debug):
+# bsub -Is -q shared_int_2h ~/screensaver/bin/screendbsynchronizer.sh <screen#>
+#
+# Screen result file must be $SCREEN_RESULTS/<screen#>_finalResults.xls
+#
+# Configure database connection arguments in $SCREENSAVER/classes/screensaver.properties
+
+SCREENSAVER_CHECKOUT=$HOME/screensaver
+SCREENSAVER=$SCREENSAVER_CHECKOUT/build/distro/screensaver
+SCREEN_RESULTS=${SCREEN_RESULTS:-$HOME/screen-results}
+
+cd $SCREENSAVER
+
+LIBS=`for s in lib/*.jar ; do printf ":$s" ; done`
+CLASSPATH="$SCREENSAVER/classes$LIBS"
+JAVA=/opt/java/jdk1.5/bin/java
+
+#echo running from `pwd`
+#echo classpath=$CLASSPATH
+$JAVA -Xmx1500m -cp $CLASSPATH edu.harvard.med.screensaver.io.screenresults.ScreenResultImporter -f $SCREEN_RESULTS/$1_finalResults.xls -s $1 -w 4 -i
+echo $0 terminated
