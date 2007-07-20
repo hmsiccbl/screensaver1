90c24a466e070b83093f3aa811d49ffedece4b1d
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-07-20 21:30:14 +0000

    cleaned imports (this once fixes a compilation error on orchestra!)
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1595 80c535d0-b110-0410-b4de-9279f94142c3

--- src/edu/harvard/med/screensaver/model/libraries/WellVolumeAdjustment.java
+++ src/edu/harvard/med/screensaver/model/libraries/WellVolumeAdjustment.java
@@ -18,8 +18,6 @@ import edu.harvard.med.screensaver.model.screens.LabCherryPick;
 
 import org.apache.log4j.Logger;
 
-import sun.security.krb5.internal.n;
-
 /**
  * A Hibernate entity bean representing a well volume adjustment. A well volume
  * adjustment normally occurs when the lab removes liquid from a library copy
