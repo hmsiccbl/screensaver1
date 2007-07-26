d3d5839ad5ceabf5df1f9a871adc2165f59578c1
js163 <js163@80c535d0-b110-0410-b4de-9279f94142c3>
2007-07-26 14:45:23 +0000

    fixed schema update to work on older versions of postgres (such as found on orchestra)
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1625 80c535d0-b110-0410-b4de-9279f94142c3

--- flotsam+jetsam/manual_schema_updates/1619.sql
+++ flotsam+jetsam/manual_schema_updates/1619.sql
@@ -7,11 +7,16 @@ current_timestamp,
 'added compound.is_pubchem_cid_list_upgrader_{failed,successful}';
  
 ALTER TABLE compound
-  ADD COLUMN is_pubchem_cid_list_upgrader_failed BOOL DEFAULT FALSE,
-  ADD COLUMN is_pubchem_cid_list_upgrader_successful BOOL DEFAULT FALSE;
+  ADD COLUMN is_pubchem_cid_list_upgrader_failed BOOL;
+ALTER TABLE compound
+  ADD COLUMN is_pubchem_cid_list_upgrader_successful BOOL;
+
+UPDATE compound SET is_pubchem_cid_list_upgrader_failed = false,
+  is_pubchem_cid_list_upgrader_successful = false;
 
 ALTER TABLE compound
-  ALTER COLUMN is_pubchem_cid_list_upgrader_failed set not null,
+  ALTER COLUMN is_pubchem_cid_list_upgrader_failed set not null;
+ALTER TABLE compound
   ALTER COLUMN is_pubchem_cid_list_upgrader_successful set not null;
 
 COMMIT;
