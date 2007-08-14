91e8bd30eb1ee952539fc17b89d8907063216f3f
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-08-14 14:27:08 +0000

    added some javadoc package-info.java files
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1701 80c535d0-b110-0410-b4de-9279f94142c3

---
+++ src/edu/harvard/med/screensaver/io/workbook2/package-info.java
@@ -0,0 +1,4 @@
+/**
+Abstraction layer around JExcelAPI Excel workbook I/O library.  Use of this package is preferred over the {@link edu.harvard.med.screensaver.io.workbook} package.
+*/
+package edu.harvard.med.screensaver.io.workbook2;
--- src/edu/harvard/med/screensaver/service/package-info.java
+++ src/edu/harvard/med/screensaver/service/package-info.java
@@ -1,10 +1,9 @@
-<html>
-<body>
-Contains packages and classes that implement discrete, independent services that read and modify data inthe Screensaver data model.  
+/**
+Contains packages and classes that implement discrete, independent services that read and modify data in the Screensaver data model.  
 Services are high-level "business" processes (well, lab processes, in our case).  Usually a business process will perform non-trivial 
 manipulation of Screensaver data, and will usually interact with disparate parts of the data model.  These services are user 
 interface-agnostic.
 
 For basic input/output functions see the {@link edu.harvard.med.screensaver.io} package (e.g. importers and exporters).
-</body>
-</html>
+*/
+package edu.harvard.med.screensaver.service;
