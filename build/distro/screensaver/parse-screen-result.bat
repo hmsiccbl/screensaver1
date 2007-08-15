9da306dd7ac32d317ff74f495a68ceecb4796dc2
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-08-15 21:40:04 +0000

    removed defunct distro scripts: parse-screen-result.{bat.sh}
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1711 80c535d0-b110-0410-b4de-9279f94142c3

--- build/distro/screensaver/parse-screen-result.bat
+++
@@ -1,3 +0,0 @@
-REM usage: parse-screen-result.bat <ScreenResultParser options>
-REM for help: parse-screen-result.bat -h
-./run.bat edu.harvard.med.screensaver.io.screenresults.ScreenResultParser %*
--- build/distro/screensaver/parse-screen-result.sh
+++
@@ -1,3 +0,0 @@
-# usage: parse-screen-result.sh <ScreenResultParser options>
-# for help: parse-screen-result.sh -h
-./run.sh edu.harvard.med.screensaver.io.screenresults.ScreenResultParser "$@"
