c1e5f7dfc7078e808f557730522df6e19aa23190
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-08-15 20:55:40 +0000

    - Removed Sun's code sharing agreement (our code is now effectively unrelated to Sun's original example code) - On login failure, detail failure message is now output to log
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1710 80c535d0-b110-0410-b4de-9279f94142c3

--- src/edu/harvard/med/screensaver/ui/authentication/ScreensaverLoginModule.java
+++ src/edu/harvard/med/screensaver/ui/authentication/ScreensaverLoginModule.java
@@ -7,47 +7,6 @@
 //at Harvard Medical School. This software is distributed under the terms of
 //the GNU General Public License.
 
-
-/*
- * Derived from Sun's SampleLoginModule.java 1.18 00/01/11
- *
- * Copyright 2000-2002 Sun Microsystems, Inc. All Rights Reserved.
- *
- * Redistribution and use in source and binary forms, with or 
- * without modification, are permitted provided that the following 
- * conditions are met:
- * 
- * -Redistributions of source code must retain the above copyright  
- * notice, this  list of conditions and the following disclaimer.
- * 
- * -Redistribution in binary form must reproduct the above copyright 
- * notice, this list of conditions and the following disclaimer in 
- * the documentation and/or other materials provided with the 
- * distribution.
- * 
- * Neither the name of Sun Microsystems, Inc. or the names of 
- * contributors may be used to endorse or promote products derived 
- * from this software without specific prior written permission.
- * 
- * This software is provided "AS IS," without a warranty of any 
- * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND 
- * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY 
- * EXCLUDED. SUN AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY 
- * DAMAGES OR LIABILITIES  SUFFERED BY LICENSEE AS A RESULT OF  OR 
- * RELATING TO USE, MODIFICATION OR DISTRIBUTION OF THE SOFTWARE OR 
- * ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE 
- * FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, 
- * SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER 
- * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF 
- * THE USE OF OR INABILITY TO USE SOFTWARE, EVEN IF SUN HAS BEEN 
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
- * 
- * You acknowledge that Software is not designed, licensed or 
- * intended for use in the design, construction, operation or 
- * maintenance of any nuclear facility. 
- */
-
 package edu.harvard.med.screensaver.ui.authentication;
 
 import java.security.Principal;
@@ -288,10 +247,10 @@ public class ScreensaverLoginModule implements LoginModule
       }
       else {
         // authentication failed, clean out state
+        String statusMessage = _authenticationResult.getStatusMessage();
         log.info("authentication failed for user '" + username + 
                  "' with status code " + _authenticationResult.getStatusCode() + 
-                 " (" + _authenticationResult.getStatusCodeCategory() + ")");
-        String statusMessage = _authenticationResult.getStatusMessage();
+                 " (" + _authenticationResult.getStatusCodeCategory() + ": '" + statusMessage + "')");
         reset(true);
         throw new FailedLoginException(statusMessage);
       }
