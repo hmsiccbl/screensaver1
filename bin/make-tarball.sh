6ae6ef0d0f839f47ef3290a7230a188f8e14ce27
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-08-24 17:57:19 +0000

    make-tarball.sh: more advanced, but broken
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1745 80c535d0-b110-0410-b4de-9279f94142c3

--- bin/make-tarball.sh
+++ bin/make-tarball.sh
@@ -1,1 +1,10 @@
-tar czf screensaver.tgz --exclude '*compound-libraries*.zip' --exclude '*/.svn*' --exclude '*/lib/*/unused*' screensaver
+# builds a tarball of screensaver project for distribution purposes, excluding necessary files (svn, downloads, etc.)
+# BROKEN, but almost there...
+exit 1;
+cd -P `dirname $0`/..
+SCREENSAVER=`pwd`
+SCREENSAVER=`basename $SCREENSAVER`
+cd `dirname $0`/..
+echo making tarball of \"$SCREENSAVER\" from `pwd`
+THIS=$SCREENSAVER/bin/`basename $0`
+tar czvf screensaver.tgz -follow --exclude '*compound-libraries*.zip' --exclude '*/.svn*' --exclude '*/lib/*/unused*' --exclude $THIS $SCREENSAVER
