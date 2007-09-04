afdaf87c7602f1fd664328e0a19603892e31b6e1
s <s@80c535d0-b110-0410-b4de-9279f94142c3>
2007-09-04 14:14:23 +0000

    granted Sean Johnston cherryPickAdmin role. (also on production database)
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1767 80c535d0-b110-0410-b4de-9279f94142c3

--- resources/sql/initialize_database/01_initialize_administrative_users.sql
+++ resources/sql/initialize_database/01_initialize_administrative_users.sql
@@ -114,6 +114,7 @@ insert into administrator_user (screensaver_user_id) values (currval('screensave
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'readEverythingAdmin';
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screensAdmin';
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'usersAdmin';
+insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'cherryPickAdmin';
 
 insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id, digested_password, ecommons_id) values (nextval('screensaver_user_id_seq'), 1, 'today', 'Stewart', 'Rudnicki', 'stewart_rudnicki@hms.harvard.edu', 'srudnicki', '2f78d8559c6e31a10fcb3e3b9eb70ef55bef02eb', 'sr50');
 insert into administrator_user (screensaver_user_id) values (currval('screensaver_user_id_seq'));
