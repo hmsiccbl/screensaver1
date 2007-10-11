9266f238b14977407f5bc6c5143b05602c4a00b8
js163 <js163@80c535d0-b110-0410-b4de-9279f94142c3>
2007-10-11 15:02:18 +0000

    fixed a schema_history.comment
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1962 80c535d0-b110-0410-b4de-9279f94142c3

--- flotsam+jetsam/manual_schema_updates/1949.sql
+++ flotsam+jetsam/manual_schema_updates/1949.sql
@@ -4,7 +4,7 @@ INSERT INTO schema_history (screensaver_revision, date_updated, comment)
 SELECT
 1948,
 current_timestamp,
-'added unique constraints for screensaver_user';
+'added unique constraint for screensaver_user.login_id';
 
 alter table screensaver_user add constraint screensaver_user_login_id_key unique (login_id);
 
