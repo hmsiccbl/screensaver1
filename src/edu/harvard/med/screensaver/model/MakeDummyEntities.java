67783921bd6266daf78c3bc807f75fd9a24d8b24
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-10-11 13:09:52 +0000

    fixed dummy entity creation (for tests), to work with ScreensaverUser unique key constraint on first,last,date
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1957 80c535d0-b110-0410-b4de-9279f94142c3

--- src/edu/harvard/med/screensaver/model/MakeDummyEntities.java
+++ src/edu/harvard/med/screensaver/model/MakeDummyEntities.java
@@ -2,7 +2,7 @@
 // $Id$
 //
 // Copyright 2006 by the President and Fellows of Harvard College.
-// 
+//
 // Screensaver is an open-source project developed by the ICCB-L and NSRB labs
 // at Harvard Medical School. This software is distributed under the terms of
 // the GNU General Public License.
@@ -28,7 +28,7 @@ public class MakeDummyEntities
   {
     return new ScreeningRoomUser(new Date(),
                                  first,
-                                 last,
+                                 last + "_" + screenNumber,
                                  first.toLowerCase() + "_" + last.toLowerCase() + "_" + screenNumber + "@hms.harvard.edu",
                                  "",
                                  "",
