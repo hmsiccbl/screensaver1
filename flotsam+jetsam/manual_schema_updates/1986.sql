cfaee17a05fb80e5ec780f17c5b0c9cac21d8ebb
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-10-19 19:47:26 +0000

    correction to 1986.sql
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@1987 80c535d0-b110-0410-b4de-9279f94142c3

--- flotsam+jetsam/manual_schema_updates/1986.sql
+++ flotsam+jetsam/manual_schema_updates/1986.sql
@@ -4,10 +4,11 @@ INSERT INTO schema_history (screensaver_revision, date_updated, comment)
 SELECT
 1986,
 current_timestamp,
-'added unique constraint for screen.screen_number; dropped unique constraint screensaver_user.screensaver_user_first_last_date_key';
+'added unique constraint for screen.screen_number';
 
 ALTER TABLE screen ADD CONSTRAINT screen_screen_number_key UNIQUE (screen_number);
 
-ALTER TABLE screensaver_user DROP CONSTRAINT screensaver_user_first_last_date_key;
+/* the following must have been removed manually on dev and prod dbs, as it isn't there */
+/*ALTER TABLE screensaver_user DROP CONSTRAINT screensaver_user_first_last_date_key;*/
 
 COMMIT;
