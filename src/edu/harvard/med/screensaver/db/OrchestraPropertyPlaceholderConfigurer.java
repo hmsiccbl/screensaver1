55f7c647d62bc44cccb9fcd824d04536bd182141
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-11-01 18:45:09 +0000

    - OrchestraPropertyPlaceholderConfigurer now uses an explicit property to find db connection settings on orchestra - updated screensaver.properties.web.{dev,prod} for above - OrchestraPropertyPlaceholderConfigurer now uses UNIX user name for database connection, if no SCREENSAVER_PGSQL_USER property is set; this allows sharing of screensaver.properties files on orchestra by multiple users, without having to edit the user name. - split screensaver.properties.util into a dev and prod version to take advantage of above
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@2013 80c535d0-b110-0410-b4de-9279f94142c3

--- src/edu/harvard/med/screensaver/db/OrchestraPropertyPlaceholderConfigurer.java
+++ src/edu/harvard/med/screensaver/db/OrchestraPropertyPlaceholderConfigurer.java
@@ -2,7 +2,7 @@
 // $Id$
 //
 // Copyright 2006 by the President and Fellows of Harvard College.
-// 
+//
 // Screensaver is an open-source project developed by the ICCB-L and NSRB labs
 // at Harvard Medical School. This software is distributed under the terms of
 // the GNU General Public License.
@@ -36,71 +36,53 @@ import org.springframework.beans.factory.config.PropertyResourceConfigurer;
 public class OrchestraPropertyPlaceholderConfigurer
 extends PropertyPlaceholderConfigurer
 {
-  
+
   // static members
 
   private static Logger log = Logger
     .getLogger(OrchestraPropertyPlaceholderConfigurer.class);
-  
-  
+
+
   // private instance fields
-  
+
   private Properties _properties;
-  
-  
+
+
   // protected instance method
 
   synchronized protected void convertProperties(Properties properties)
   {
     _properties = properties;
-    if (shouldConvertPropertiesFromOrchestraAuthFile()) {
-      convertPropertiesFromOrchestraAuthFile();
+    if (convertPropertiesFromOrchestraAuthFile()) {
+      return;
     }
-    else if (shouldReadPasswordFromDotPgpassFile()) {
+    if (shouldUseUnixUser()) {
+      useUnixUser();
+    }
+    if (shouldReadPasswordFromDotPgpassFile()) {
       readPasswordFromDotPgpassFile();
     }
   }
-  
+
   private boolean shouldConvertPropertiesFromOrchestraAuthFile()
   {
-    boolean shouldConvert = _properties.getProperty("SCREENSAVER_PGSQL_SERVER") == null;
+    boolean shouldConvert = _properties.getProperty("orchestra.db.connection.file") == null;
     if (shouldConvert) {
-      log.info("Found SCREENSAVER_PGSQL_SERVER env var: assuming we are running on orchestra");
-      // we have null values for the required datasource properties, so assume we are on
-      // orchestra, and apply orchestra-specific method for obtaining them      
     }
     return shouldConvert;
   }
 
-  private void convertPropertiesFromOrchestraAuthFile() {
-    String catalinaBase = System.getenv("CATALINA_BASE");
-    log.info("CATALINA_BASE env var=" + catalinaBase);
-    if (catalinaBase == null || ! catalinaBase.matches("/www/[^/]+/tomcat")) {
-      log.warn("We (admittedly not so cleverly) assumed that since the property " +
-        "\"SCREENSAVER_PGSQL_SERVER\" was not set, either in screensaver.properties, or " +
-        "in the environment variables passed to the JVM, that this was an instance " +
-        "of Screensaver running on the orchestra webserver \"trumpet\". However, we " +
-        "also expect such instances to have a \"CATALINA_BASE\" environment variable " +
-        "passed to the JVM, whose value starts with \"/www/\", and ends with " +
-        "\"/tomcat\", and whose remainder is the site name. Unfortunately, our " +
-        "expectations in regards to the \"CATALINA_BASE\" environment variable " +
-        "failed to hold. In this situation, we are afraid we have absolutely no way of " +
-        "being able to appropriately set the datasource properties necessary for " +
-        "Screensaver to run. (The value we found for the \"CATALINA_BASE\" environment " +
-        "variable is \"" + catalinaBase + "\".)");
-
-      // well, it turns out we are not actually on orchestra, so give up 
-      return;
+  private boolean convertPropertiesFromOrchestraAuthFile() {
+    String orchestraAuthFilename = _properties.getProperty("orchestra.db.connection.file");
+    if (orchestraAuthFilename == null) {
+      return false;
     }
-
-    // obscurely trim the "/www/" and "/tomcat" prefix and postfix to get the site name
-    String orchestraSite = catalinaBase.substring(5, catalinaBase.length() - 7);
-    String authFilename = "/opt/apache/conf/auth/" + orchestraSite;
+    log.info("using orchestra database connection settings from file " + orchestraAuthFilename);
 
     // parse the datasource properties out of the file
     try {
       Pattern pattern = Pattern.compile("SetEnv\\s+(\\S+)\\s+(\\S+)");
-      File authFile = new File(authFilename);
+      File authFile = new File(orchestraAuthFilename);
       FileInputStream authFileInputStream = new FileInputStream(authFile);
       InputStreamReader authInputStreamReader = new InputStreamReader(authFileInputStream);
       BufferedReader authBufferedReader = new BufferedReader(authInputStreamReader);
@@ -114,17 +96,36 @@ extends PropertyPlaceholderConfigurer
         }
         line = authBufferedReader.readLine();
       }
+      return true;
     }
     catch (IOException e) {
       log.warn("unable to read and parse the orchestra auth file", e);
+      return false;
     }
   }
-  
+
+  private boolean shouldUseUnixUser()
+  {
+    return _properties.getProperty("SCREENSAVER_PGSQL_USER") == null;
+  }
+
+  private void useUnixUser()
+  {
+    String unixUser = System.getenv("$USER");
+    if (unixUser == null) {
+      log.warn("could not determine UNIX user name");
+    }
+    else {
+      _properties.setProperty("SCREENSAVER_PGSQL_USER", unixUser);
+      log.info("using UNIX user name '" + unixUser + "'");
+    }
+  }
+
   private boolean shouldReadPasswordFromDotPgpassFile()
   {
     return _properties.getProperty("SCREENSAVER_PGSQL_PASSWORD") == null;
   }
-  
+
   private void readPasswordFromDotPgpassFile()
   {
     _properties.setProperty(
