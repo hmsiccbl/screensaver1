1a89df2d60d950b5b5498860266d5e97120f7369
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-11-01 19:53:58 +0000

    update screendb_synchronizer.sh to work with updated build system
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@2016 80c535d0-b110-0410-b4de-9279f94142c3

--- bin/io/screendb_synchronizer.sh
+++ bin/io/screendb_synchronizer.sh
@@ -9,17 +9,16 @@
 #   - put a timestamp or something in the filenames so they dont overwrite
 #   - check for existence of files before overwriting?
 
-SCREENSAVER_CHECKOUT=$HOME/screensaver
-BUILD=$SCREENSAVER_CHECKOUT/build
+SCREENSAVER=`dirname $0`/../..
+cd $SCREENSAVER
+SCREENSAVER=`pwd -P`
 
-cd $SCREENSAVER_CHECKOUT
-
-LIBS=`for s in $BUILD/lib/*.jar ; do printf ":$s" ; done`
-CLASSPATH="$BUILD/classes$LIBS"
+LIBS=`for s in $SCREENSAVER/lib/*.jar ; do printf ":$s" ; done`
+CLASSPATH="$SCREENSAVER/classes$LIBS"
 JAVA=/opt/java/jdk1.5/bin/java
 
 $JAVA -Xmx1500m -cp $CLASSPATH edu.harvard.med.screensaver.db.screendb.OrchestraStandaloneScreenDBSynchronizer -S pgsql.cl.med.harvard.edu -D screendb -U $USER > screendb_synchronizer.out 2> screendb_synchronizer.err
 
 echo screendb_synchronizer.sh is complete
-echo program output is in $SCREENSAVER_CHECKOUT/screendb_synchronizer.{out,err}
+echo program output is in $SCREENSAVER/screendb_synchronizer.{out,err}
 
