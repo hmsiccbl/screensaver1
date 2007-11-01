82752aee5df975ba8e8f54d6df5c635e420158f7
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-11-01 15:01:59 +0000

    reworked 2002.sql to convert result_value_type_result_value to result_value by means of table and column renaming instead of table create-copy-drop strategy, which choked postgres.  Only runs on posgres 8.x+.
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@2003 80c535d0-b110-0410-b4de-9279f94142c3

--- flotsam+jetsam/manual_schema_updates/2002.sql
+++ flotsam+jetsam/manual_schema_updates/2002.sql
@@ -4,21 +4,30 @@ INSERT INTO schema_history (screensaver_revision, date_updated, comment)
 SELECT
 2002,
 current_timestamp,
-'convert ResultValue into an entity; ';
+'convert ResultValue into an entity';
 
+ALTER TABLE study_reagent_link DROP CONSTRAINT FK352B361D161EA629;
+ALTER TABLE study_reagent_link
+    ADD CONSTRAINT fk_reagent_to_study
+    FOREIGN KEY (reagent_id)
+    REFERENCES reagent;
 
-CREATE TABLE result_value (
-    result_value_id INT4 NOT NULL,
-    assay_well_type TEXT NOT NULL,
-    is_exclude BOOL NOT NULL,
-    numeric_decimal_precision INT4,
-    numeric_value FLOAT8,
-    is_positive BOOL NOT NULL,
-    value TEXT,
-    result_value_type_id INT4 NOT NULL,
-    well_id TEXT NOT NULL,
-    PRIMARY KEY (result_value_id)
-);
+CREATE SEQUENCE result_value_id_seq;
+
+ALTER TABLE result_value_type_result_values DROP CONSTRAINT fkbfc506afeead059e;
+ALTER TABLE result_value_type_result_values DROP CONSTRAINT result_value_type_result_values_well_id_fkey;
+ALTER TABLE result_value_type_result_values DROP CONSTRAINT result_value_type_result_values_pkey;
+DROP INDEX rvt_index;
+DROP INDEX wellkey_index;
+ALTER TABLE result_value_type_result_values RENAME TO result_value;
+ALTER TABLE result_value ADD COLUMN result_value_id INT4;
+UPDATE result_value set result_value_id = nextval('result_value_id_seq');
+ALTER TABLE result_value ALTER COLUMN result_value_id SET NOT NULL;
+ALTER TABLE result_value ADD PRIMARY KEY (result_value_id);
+ALTER TABLE result_value RENAME COLUMN result_value_type TO result_value_type_id;
+CREATE INDEX result_value_well_index ON result_value (well_id);
+CREATE INDEX result_value_result_value_type_index ON result_value (result_value_type_id);
+CREATE INDEX result_value_is_positive_index ON result_value (is_positive);
 
 ALTER TABLE result_value
     ADD CONSTRAINT fk_result_value_to_result_value_type
@@ -29,23 +38,5 @@ ALTER TABLE result_value
     ADD CONSTRAINT fk_result_value_to_well
     FOREIGN KEY (well_id)
     REFERENCES well;
-
-ALTER TABLE study_reagent_link DROP CONSTRAINT FK352B361D161EA629;
-ALTER TABLE study_reagent_link
-    ADD CONSTRAINT fk_reagent_to_study
-    FOREIGN KEY (reagent_id)
-    REFERENCES reagent;
-
-CREATE SEQUENCE result_value_id_seq;
-
-INSERT INTO result_value (result_value_id, assay_well_type, is_exclude, numeric_decimal_precision, numeric_value, is_positive, value, result_value_type_id, well_id) SELECT nextval('result_value_id_seq'), assay_well_type, is_exclude, numeric_decimal_precision, numeric_value, is_positive, value, result_value_type, well_id FROM result_value_type_result_values;
-
-CREATE INDEX result_value_well_index ON result_value (well_id);
-
-CREATE INDEX result_value_result_value_type_index ON result_value (result_value_type_id);
-
-CREATE INDEX result_value_is_positive_index ON result_value (is_positive);
-
-DROP TABLE result_value_type_result_values;
 
 COMMIT;
