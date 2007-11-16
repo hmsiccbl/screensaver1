a90f6e0f27c1141d97d6c8553ae982dd38d331e9
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2007-11-16 16:09:02 +0000

    EntitySearchResult ensures that "table filter mode" is always disabled when in "entity view" mode, since search fields would entirely hidden from user in this mode.
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@2040 80c535d0-b110-0410-b4de-9279f94142c3

--- src/edu/harvard/med/screensaver/ui/searchresults/EntitySearchResults.java
+++ src/edu/harvard/med/screensaver/ui/searchresults/EntitySearchResults.java
@@ -1,11 +1,13 @@
-//$HeadURL: svn+ssh://js163@orchestra.med.harvard.edu/svn/iccb/screensaver/trunk/.eclipse.prefs/codetemplates.xml $
-//$Id: codetemplates.xml 169 2006-06-14 21:57:49Z js163 $
+// $HeadURL:
+// svn+ssh://js163@orchestra.med.harvard.edu/svn/iccb/screensaver/trunk/.eclipse.prefs/codetemplates.xml
+// $
+// $Id: codetemplates.xml 169 2006-06-14 21:57:49Z js163 $
 
-//Copyright 2006 by the President and Fellows of Harvard College.
+// Copyright 2006 by the President and Fellows of Harvard College.
 
-//Screensaver is an open-source project developed by the ICCB-L and NSRB labs
-//at Harvard Medical School. This software is distributed under the terms of
-//the GNU General Public License.
+// Screensaver is an open-source project developed by the ICCB-L and NSRB labs
+// at Harvard Medical School. This software is distributed under the terms of
+// the GNU General Public License.
 
 package edu.harvard.med.screensaver.ui.searchresults;
 
@@ -34,8 +36,8 @@ import org.apache.log4j.Logger;
 /**
  * SearchResults subclass that adds support for:
  * <ul>
- * <li>"Summary" and "Entity" viewing modes, corresponding to a multi-entity list
- * view and a single-entity full page view, respectively.</li>
+ * <li>"Summary" and "Entity" viewing modes, corresponding to a multi-entity
+ * list view and a single-entity full page view, respectively.</li>
  * <li>Downloading of search results via one or more {@link DataExporter}s.</li>
  * </ul>
  *
@@ -50,17 +52,14 @@ public abstract class EntitySearchResults<E extends AbstractEntity> extends Sear
 
   private static final String[] CAPABILITIES = { "viewEntity", "exportData", "filter" };
 
-
   // instance data members
 
   private List<DataExporter<E>> _dataExporters = Collections.emptyList();
   private UISelectOneBean<DataExporter<E>> _dataExporterSelector;
 
-
   // abstract methods
 
   abstract protected void setEntityToView(E entity);
-
 
   // protected methods
 
@@ -70,8 +69,10 @@ public abstract class EntitySearchResults<E extends AbstractEntity> extends Sear
    */
   final protected String viewCurrentEntity()
   {
-    getDataTable().getRowsPerPageSelector().setSelection(1);
-    getDataTable().gotoRowIndex(getDataTable().getDataModel().getRowIndex());
+    getDataTable().getRowsPerPageSelector()
+                  .setSelection(1);
+    getDataTable().gotoRowIndex(getDataTable().getDataModel()
+                                              .getRowIndex());
     updateEntityToView();
     return REDISPLAY_PAGE_ACTION_RESULT;
   }
@@ -81,11 +82,13 @@ public abstract class EntitySearchResults<E extends AbstractEntity> extends Sear
   protected DataModel buildDataModel()
   {
     DataModel dataModel = super.buildDataModel();
-    dataModel.addDataModelListener(new DataModelListener() {
+    dataModel.addDataModelListener(new DataModelListener()
+    {
       @SuppressWarnings("unchecked")
       public void rowSelected(DataModelEvent event)
       {
-        assert event.getRowData() == getDataTable().getDataModel().getRowData();
+        assert event.getRowData() == getDataTable().getDataModel()
+                                                   .getRowData();
         updateEntityToView();
       }
     });
@@ -94,9 +97,14 @@ public abstract class EntitySearchResults<E extends AbstractEntity> extends Sear
 
   final protected DataTableRowsPerPageUISelectOneBean buildRowsPerPageSelector()
   {
-    // note: we need a special "single" (1) selection item, for viewing the entity in its full viewer page
-    DataTableRowsPerPageUISelectOneBean rowsPerPageSelector =
-      new DataTableRowsPerPageUISelectOneBean(Arrays.asList(1, 10, 20, 50, 100), 20)
+    // note: we need a special "single" (1) selection item, for viewing the
+    // entity in its full viewer page
+    DataTableRowsPerPageUISelectOneBean rowsPerPageSelector = new DataTableRowsPerPageUISelectOneBean(Arrays.asList(1,
+                                                                                                                    10,
+                                                                                                                    20,
+                                                                                                                    50,
+                                                                                                                    100),
+                                                                                                      20)
     {
       @Override
       public String getLabel(Integer value)
@@ -110,7 +118,8 @@ public abstract class EntitySearchResults<E extends AbstractEntity> extends Sear
       }
     };
 
-    rowsPerPageSelector.addObserver(new Observer() {
+    rowsPerPageSelector.addObserver(new Observer()
+    {
       public void update(Observable obs, Object o)
       {
         if (((Integer) o) == 1) {
@@ -121,7 +130,6 @@ public abstract class EntitySearchResults<E extends AbstractEntity> extends Sear
 
     return rowsPerPageSelector;
   }
-
 
   // public constructors and methods
 
@@ -140,17 +148,20 @@ public abstract class EntitySearchResults<E extends AbstractEntity> extends Sear
   }
 
   @Override
-  public void setContents(Collection<? extends E> unsortedResults, String description)
+  public void setContents(Collection<? extends E> unsortedResults,
+                          String description)
   {
     super.setContents(unsortedResults, description);
 
-    // intercept re-sorts, to update the entity being viewed in "entity view" mode
-    getDataTable().addObserver(new Observer() {
-      public void update(Observable obs, Object o)
-      {
-        updateEntityToView();
-      }
-    });
+    // intercept re-sorts, to update the entity being viewed in "entity view"
+    // mode
+    getDataTable().addObserver(new Observer()
+                  {
+                    public void update(Observable obs, Object o)
+                    {
+                      updateEntityToView();
+                    }
+                  });
   }
 
   public boolean isSummaryView()
@@ -160,7 +171,34 @@ public abstract class EntitySearchResults<E extends AbstractEntity> extends Sear
 
   public boolean isEntityView()
   {
-    return !isSummaryView();
+    return getDataTable().getRowsPerPage() == 1 && getDataTable().getRowCount() > 0;
+  }
+
+  /**
+   * Override to ensure that "table filter mode" is always disabled when in
+   * "entity view" mode, since search fields would entirely hidden from user in
+   * this mode.
+   */
+  @Override
+  public boolean isTableFilterMode()
+  {
+    return super.isTableFilterMode() && !isEntityView();
+  }
+
+  /**
+   * Override to ensure that "table filter mode" is always disabled when in
+   * "entity view" mode, since search fields would entirely hidden from user in
+   * this mode.
+   */
+  @Override
+  public void setTableFilterMode(boolean isTableFilterMode)
+  {
+    if (isEntityView()) {
+      return;
+    }
+    else {
+      super.setTableFilterMode(isTableFilterMode);
+    }
   }
 
   public List<DataExporter<E>> getDataExporters()
@@ -168,11 +206,11 @@ public abstract class EntitySearchResults<E extends AbstractEntity> extends Sear
     return _dataExporters;
   }
 
-
   public UISelectOneBean<DataExporter<E>> getDataExporterSelector()
   {
     if (_dataExporterSelector == null) {
-      _dataExporterSelector = new UISelectOneBean<DataExporter<E>>(getDataExporters()) {
+      _dataExporterSelector = new UISelectOneBean<DataExporter<E>>(getDataExporters())
+      {
         @Override
         protected String getLabel(DataExporter<E> dataExporter)
         {
@@ -185,7 +223,7 @@ public abstract class EntitySearchResults<E extends AbstractEntity> extends Sear
 
   @SuppressWarnings("unchecked")
   @UIControllerMethod
-  /*final (CGLIB2 restriction)*/ public String downloadSearchResults()
+  /* final (CGLIB2 restriction) */public String downloadSearchResults()
   {
     try {
       DataExporter dataExporter = getDataExporterSelector().getSelection();
@@ -200,7 +238,6 @@ public abstract class EntitySearchResults<E extends AbstractEntity> extends Sear
     }
     return REDISPLAY_PAGE_ACTION_RESULT;
   }
-
 
   // private methods
 
@@ -218,7 +255,8 @@ public abstract class EntitySearchResults<E extends AbstractEntity> extends Sear
   private void updateEntityToView()
   {
     if (isEntityView()) {
-      if (getDataTable().getDataModel().isRowAvailable()) {
+      if (getDataTable().getDataModel()
+                        .isRowAvailable()) {
         E rowData = (E) getRowData();
         if (rowData != null && rowData != entityToView) {
           log.debug("setting entity for single-view mode: " + rowData);
