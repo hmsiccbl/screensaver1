1bccaf68b15a35c7828c13beeac419521d9482da
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2008-01-09 18:29:19 +0000

    added Zac Cooper as admin user
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@2095 80c535d0-b110-0410-b4de-9279f94142c3

--- resources/sql/initialize_database/01_initialize_administrative_users.sql
+++ resources/sql/initialize_database/01_initialize_administrative_users.sql
@@ -151,4 +151,8 @@ insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_ro
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screenResultsAdmin';
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'cherryPickAdmin';
 
+insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id, digested_password, ecommons_id) values (nextval('screensaver_user_id_seq'), 1, 'today', 'Zac', 'Cooper', 'zwcoop@gmail.com', null, null, 'zc32');
+insert into administrator_user (screensaver_user_id) values (currval('screensaver_user_id_seq'));
+insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'readEverythingAdmin';
+
 commit;
