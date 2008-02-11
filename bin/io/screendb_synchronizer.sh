19e0522b31045f625d2961d2ca2c374778945017
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2008-02-11 13:34:41 +0000

    all output of screendb_synchronizer.sh is tee'd to stdout and screendb_synchronizer.out, ensuring that errors are emailed to admin when run via LSF cluster
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@2173 80c535d0-b110-0410-b4de-9279f94142c3

--- bin/io/screendb_synchronizer.sh
+++ bin/io/screendb_synchronizer.sh
@@ -2,14 +2,12 @@
 #
 # run this proggie something like this:
 #
-# bsub $SCREENSAVER/bin/io/screendbsynchronizer.sh
+# bsub /home/js163/screensaver/bin/screendbsynchronizer.sh
 #
 # TODO: improve placement of output and error files:
 #   - some place secure?
 #   - put a timestamp or something in the filenames so they dont overwrite
 #   - check for existence of files before overwriting?
-#
-# Configure database connection arguments in $SCREENSAVER/classes/screensaver.properties
 
 SCREENSAVER=`dirname $0`/../..
 cd $SCREENSAVER
@@ -19,12 +17,7 @@ LIBS=`for s in $SCREENSAVER/lib/*.jar ; do printf ":$s" ; done`
 CLASSPATH="$SCREENSAVER/classes$LIBS"
 JAVA=/opt/java/jdk1.5/bin/java
 
-$JAVA -Xmx1500m -cp $CLASSPATH \
-    edu.harvard.med.screensaver.db.screendb.OrchestraStandaloneScreenDBSynchronizer \ 
-    -S pgsql.cl.med.harvard.edu -D screendb -U $USER \ 
-    > screendb_synchronizer.out \
-    2> screendb_synchronizer.err
+$JAVA -Xmx1500m -cp $CLASSPATH edu.harvard.med.screensaver.db.screendb.OrchestraStandaloneScreenDBSynchronizer -S pgsql.cl.med.harvard.edu -D screendb -U $USER 2>&1 | tee screendb_synchronizer.out
 
 echo screendb_synchronizer.sh is complete
-echo program output is in $SCREENSAVER/screendb_synchronizer.{out,err}
-
+echo program output is in $SCREENSAVER/screendb_synchronizer.out
