39556c6f56ba56efb735eaaf92f943962bb32d3d
js163 <js163@80c535d0-b110-0410-b4de-9279f94142c3>
2008-02-14 19:45:42 +0000

    updated Jenn Nail's eCommons Identity
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@2185 80c535d0-b110-0410-b4de-9279f94142c3

--- resources/sql/initialize_database/01_initialize_administrative_users.sql
+++ resources/sql/initialize_database/01_initialize_administrative_users.sql
@@ -143,7 +143,7 @@ insert into screensaver_user (screensaver_user_id, version, date_created, first_
 insert into administrator_user (screensaver_user_id) values (currval('screensaver_user_id_seq'));
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'readEverythingAdmin';
 
-insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id, digested_password, ecommons_id) values (nextval('screensaver_user_id_seq'), 1, 'today', 'Jenn', 'Nale', 'jennifernale@yahoo.com', 'jnale', '04df0c21fb1a3174d05b533705c88851fb07b3ba', 'jn60');
+insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id, digested_password, ecommons_id) values (nextval('screensaver_user_id_seq'), 1, 'today', 'Jenn', 'Nale', 'jennifernale@yahoo.com', 'jnale', '04df0c21fb1a3174d05b533705c88851fb07b3ba', 'jbn6');
 insert into administrator_user (screensaver_user_id) values (currval('screensaver_user_id_seq'));
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'readEverythingAdmin';
 insert into screensaver_user_role_type (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'librariesAdmin';
