85d1f6190eb6449a36e4774544e3ae9e70662c72
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2008-04-24 21:11:40 +0000

    CompoundPubchemCidListUpgrader improvements.  Reduces requests for compounds in multiple wells.  Makes no changes to compound if an error occurs (for any its wells' smiles).
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@2318 80c535d0-b110-0410-b4de-9279f94142c3

--- src/edu/harvard/med/screensaver/io/libraries/compound/upgraders/CompoundPubchemCidListUpgrader.java
+++ src/edu/harvard/med/screensaver/io/libraries/compound/upgraders/CompoundPubchemCidListUpgrader.java
@@ -9,8 +9,11 @@
 
 package edu.harvard.med.screensaver.io.libraries.compound.upgraders;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import edu.harvard.med.screensaver.CommandLineApplication;
 import edu.harvard.med.screensaver.db.DAOTransaction;
@@ -71,55 +74,64 @@ public class CompoundPubchemCidListUpgrader
     List<Compound> nonUpgradedCompoundsList = _dao.findEntitiesByHql(
       Compound.class,
       "from Compound c left join c.pubchemCids p where p is null");
-    log.info(
-      "retrieved " + nonUpgradedCompoundsList.size() +
-      " compounds needing an upgrade");
+    log.info("retrieved " + nonUpgradedCompoundsList.size() + " compounds needing an upgrade");
     final Iterator<Compound> nonUpgradedCompounds =
       nonUpgradedCompoundsList.iterator();
     while (nonUpgradedCompounds.hasNext()) {
-      _dao.doInTransaction(new DAOTransaction()
-      {
-        public void runTransaction()
-        {
-          int numCompoundsProcessed = 0;
-          int numCompoundsUpgraded = 0;
-          int numErrors = 0;
-          while (nonUpgradedCompounds.hasNext()) {
-            Compound compound = _dao.reloadEntity(nonUpgradedCompounds.next());
-            for (Well well : compound.getWells()) {
-              try {
-                String standardizedSmiles = pubchemSmilesOrInchiStandardizer.getPubchemStandardizedSmilesOrInchi(well.getSmiles(), CompoundIdType.SMILES);
-                if (!standardizedSmiles.equals(well.getSmiles())) {
-                  log.debug("standardized smiles from " + well.getSmiles() + " to " + standardizedSmiles);
-                }
-                List<String> pubchemCids = pubchemSmilesOrInchiSearch.getPubchemCidsForSmilesOrInchi(standardizedSmiles);
-                for (String pubchemCid : pubchemCids) {
-                  log.debug("adding new Pubchem CID " + pubchemCid);
+      int numCompoundsProcessed = 0;
+      int numCompoundsUpgraded = 0;
+      int numErrors = 0;
+      while (nonUpgradedCompounds.hasNext()) {
+        try {
+          final Compound compound = _dao.findEntityById(Compound.class, 
+                                                        nonUpgradedCompounds.next().getEntityId(),
+                                                        true,
+                                                        "wells");
+          ++numCompoundsProcessed;
+          Set<String> smilesSet = new HashSet<String>();
+          for (Well well : compound.getWells()) {
+            smilesSet.add(well.getSmiles());
+          }
+          Set<String> newPubchemCids = new HashSet<String>();
+          for (String smiles : smilesSet) {
+            String standardizedSmiles = pubchemSmilesOrInchiStandardizer.getPubchemStandardizedSmilesOrInchi(smiles, CompoundIdType.SMILES);
+            if (!standardizedSmiles.equals(smiles)) {
+              log.debug("standardized smiles from " + smiles + " to " + standardizedSmiles);
+            }
+            newPubchemCids.addAll(pubchemSmilesOrInchiSearch.getPubchemCidsForSmilesOrInchi(standardizedSmiles));
+          }
+          if (newPubchemCids.size() > 0) {
+            ++numCompoundsUpgraded;
+            for (final String pubchemCid : newPubchemCids) {
+              _dao.doInTransaction(new DAOTransaction() {
+                public void runTransaction()
+                {
+                  _dao.reattachEntity(compound);
                   compound.addPubchemCid(pubchemCid);
                 }
-                if (pubchemCids.size() > 0) {
-                  ++numCompoundsUpgraded;
-                }
-              }
-              catch (EutilsException e) {
-                ++numErrors;
-                log.error(e);
-              }
-              finally {
-                ++numCompoundsProcessed;
-                // save progress in batches, and reduce memory usage
-                if (numCompoundsProcessed % NUM_COMPOUNDS_UPGRADED_PER_TRANSACTION == 0) {
-                  _dao.flush();
-                  _dao.clear();
-                  log.info("processed " + numCompoundsProcessed + 
-                           "; upgraded " + numCompoundsUpgraded + " (" + String.format("%.2f%%", numCompoundsUpgraded / (double) numCompoundsProcessed) + "); " +
-                           "; errors " + numErrors + " (" + String.format("%.2f%%", numErrors / (double) numCompoundsProcessed) + ") ");
-                }
-              }
+              });
+              log.debug("added new Pubchem CID " + pubchemCid + " to compound " + compound.getCompoundId());
             }
           }
+          else {
+            log.debug("no new Pubchem CIDs found for compound " + compound.getCompoundId());
+          }
         }
-      });
+        catch (EutilsException e) {
+          ++numErrors;
+          log.error(e);
+        }
+        finally {
+          // save progress in batches, and reduce memory usage
+          if (numCompoundsProcessed % NUM_COMPOUNDS_UPGRADED_PER_TRANSACTION == 0) {
+            _dao.flush();
+            _dao.clear();
+            log.info("processed " + numCompoundsProcessed + 
+                     "; upgraded " + numCompoundsUpgraded + " (" + String.format("%.2f%%", numCompoundsUpgraded / (double) numCompoundsProcessed) + ")" +
+                     "; errors " + numErrors + " (" + String.format("%.2f%%", numErrors / (double) numCompoundsProcessed) + ") ");
+          }
+        }
+      }
     }
   }
 }
