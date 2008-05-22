679c7464b5f3241f5f10c9f341c210e9b26fdcd3
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2008-05-22 17:53:27 +0000

    Updated version to 1.6.1
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@2437 80c535d0-b110-0410-b4de-9279f94142c3

--- src/edu/harvard/med/screensaver/ScreensaverConstants.java
+++ src/edu/harvard/med/screensaver/ScreensaverConstants.java
@@ -16,7 +16,7 @@ public interface ScreensaverConstants
   // static data members
 
   public static final String APPLICATION_NAME = "Screensaver";
-  public static final String APPLICATION_VERSION = "1.6.0";
+  public static final String APPLICATION_VERSION = "1.6.1";
   public static final String APPLICATION_TITLE = APPLICATION_NAME + " " + APPLICATION_VERSION;
   public static final String FEEDBACK_URL = "mailto:screensaver-feedback@hms.harvard.edu";
   public static final String BUILD_NUMBER_FILE = "/build-number.txt";
