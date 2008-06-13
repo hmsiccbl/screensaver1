f099fa3881ee97c3327b5eed04433515f66a7ba5
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2008-06-13 16:09:34 +0000

    fixed 2481.sql
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@2497 80c535d0-b110-0410-b4de-9279f94142c3

--- flotsam+jetsam/manual_schema_updates/2481.sql
+++ flotsam+jetsam/manual_schema_updates/2481.sql
@@ -11,7 +11,7 @@ ALTER TABLE billing_information ADD COLUMN see_comments BOOL;
 UPDATE billing_information SET to_be_requested = CASE WHEN billing_info_to_be_requested = 'Yes' THEN true ELSE false END;
 UPDATE billing_information SET see_comments = CASE WHEN billing_info_to_be_requested = 'No, see comments' THEN true ELSE false END;
 ALTER TABLE billing_information ALTER COLUMN to_be_requested SET NOT NULL;
-ALTER TABLE billing_information ALTER COLUMN see_comment SET NOT NULL;
+ALTER TABLE billing_information ALTER COLUMN see_comments SET NOT NULL;
 ALTER TABLE billing_information DROP COLUMN billing_info_to_be_requested;
 
 COMMIT;
