621d497f4b047d7cdd9e4486925985c3f392d6dd
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2008-07-30 18:27:35 +0000

    run.sh no longer needs to care about 'unused/' lib directories
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@2657 80c535d0-b110-0410-b4de-9279f94142c3

--- bin/run.sh
+++ bin/run.sh
@@ -5,7 +5,7 @@ SCREENSAVER=.
 JAVA=$JAVA_HOME/bin/java 
 MAX_RAM_MB=800
 CLASSES=$SCREENSAVER/classes
-JARS=`find $SCREENSAVER/lib \( -name unused -prune \) -or -name "*.jar" -print`
+JARS=`find $SCREENSAVER/lib -name "*.jar" -print`
 LIBS=`for s in $JARS ; do printf ":$s" ; done`
 CLASSPATH=$CLASSES:$LIBS
 $JAVA -Xmx${MAX_RAM_MB}m -cp $CLASSPATH "$@"
