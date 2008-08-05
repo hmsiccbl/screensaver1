213dc667e24090d2d0c52fe46f9f5138cba9ca5f
ant4 <ant4@80c535d0-b110-0410-b4de-9279f94142c3>
2008-08-05 12:03:53 +0000

    updates to example-database/ files
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@2678 80c535d0-b110-0410-b4de-9279f94142c3

--- database/example-database/initialize_example_users.sql
+++ database/example-database/initialize_example_users.sql
@@ -1,7 +1,17 @@
-/* Creates a 'dev' (developer) and 'guest' account with passwords same as login ID. */
+/* 
+ * Creates a 'guest' account and a 'dev' (developer) account, with
+ * passwords same as login ID. You may copy these two examples to
+ * create additional users, as desired.  Note that the 'dev'
+ * administrator account includes the special 'developer' role, which
+ * is generally not needed when creating normal administrator
+ * accounts.  The "digested" password value is a SHA1 cryptographic
+ * hash value of the plaintext password (there are many online SHA
+ * calculators you can use to generate these values.)
+ */
 
 begin;
 
+/* Create a 'guest' user account. */
 insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id, digested_password) values (nextval('screensaver_user_id_seq'), 1, 'today', 'Screensaver', 'Guest', '', 'guest', '35675e68f4b5af7b995d9205ad0fc43842f16450');
 insert into screening_room_user (screensaver_user_id, user_classification) values (currval('screensaver_user_id_seq'), 'Other');
 insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screensaverUser';
@@ -9,6 +19,7 @@ insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) s
 insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'compoundScreeningRoomUser';
 insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'rnaiScreeningRoomUser';
 
+/* Create a 'dev' administrator account. */
 insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id, digested_password) values (nextval('screensaver_user_id_seq'), 1, 'today', 'Screensaver', 'Developer', '', 'dev', '34c6fceca75e456f25e7e99531e2425c6c1de443');
 insert into administrator_user (screensaver_user_id) values (currval('screensaver_user_id_seq'));
 insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screensaverUser';
