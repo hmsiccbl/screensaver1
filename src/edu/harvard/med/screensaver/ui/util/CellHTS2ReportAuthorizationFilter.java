840293bd01733e1a49ebbf3ba309fa6a538a723d
awsc <awsc@80c535d0-b110-0410-b4de-9279f94142c3>
2008-09-25 01:29:04 +0000

    (Changes from collaboration branch) Servlet filter for cellHTS2 report authorization [#159853]  - This filter checks whether the logged-in user has the rights to view the screenresult before he/she can view the cellHTS2 report. If the user is restricted from viewing, he/she will be redirected to the cellHTS2ReportAccessDenied page.  - Similarly for cases where the ScreenViewer has not been visited (refer to remark below), the user will be redirected to the same page as above.
    Remark from Andrew:
     It checks the access restrictions on the screen being currently viewed (as of the previous HTTP request, meaning that ScreenViewer must have already been visited by the user)
    
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@2745 80c535d0-b110-0410-b4de-9279f94142c3

---
+++ src/edu/harvard/med/screensaver/ui/util/CellHTS2ReportAuthorizationFilter.java
@@ -0,0 +1,75 @@
+package edu.harvard.med.screensaver.ui.util;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import edu.harvard.med.screensaver.model.screenresults.ScreenResult;
+import edu.harvard.med.screensaver.model.screens.Screen;
+import edu.harvard.med.screensaver.ui.screens.ScreenViewer;
+
+/**
+* Servlet filter for cellHTS2 report authorization
+* @author siew cheng (BII). Thanks to Andrew for the code to retrieve bean from session.
+* 
+*/
+
+public class CellHTS2ReportAuthorizationFilter extends OncePerRequestFilter {
+
+  //static data members
+
+  private static Logger log = Logger.getLogger(CellHTS2ReportAuthorizationFilter.class);
+
+  private static final String CELLHTS2_REPORT_ACCESS_DENIED_URL = "/screensaver/main/cellHTS2ReportAccessDenied.jsf";
+
+
+  //instance data members
+
+  //methods
+
+  protected void doFilterInternal(final HttpServletRequest request, 
+                                  final HttpServletResponse response, 
+                                  final FilterChain filterChain)
+  throws ServletException, IOException 
+  {
+
+    final HttpSession httpSession = request.getSession();
+
+    ScreenViewer bean = ((ScreenViewer) httpSession.getAttribute("scopedTarget.screenViewer"));
+    try {
+      if (bean != null) {
+        Screen screen = bean.getScreen();
+        if (screen != null) {
+          ScreenResult screenResult = screen.getScreenResult();
+          if (screenResult != null && !screenResult.isRestricted()) {
+            log.info("user authorized to view screen result for screen " + screen.getScreenNumber());
+          }
+          else {
+            log.info("user not authorized to view screen result for screen " + screen.getScreenNumber());
+            response.sendRedirect(CELLHTS2_REPORT_ACCESS_DENIED_URL);
+          }
+        }
+        else {
+          log.info("can't get screen from screenViewer in http session");
+          response.sendRedirect(CELLHTS2_REPORT_ACCESS_DENIED_URL);
+        }
+      }
+      else {
+        log.info("screenViewer not in http session");
+        response.sendRedirect(CELLHTS2_REPORT_ACCESS_DENIED_URL);
+      }
+
+      filterChain.doFilter(request, response);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
