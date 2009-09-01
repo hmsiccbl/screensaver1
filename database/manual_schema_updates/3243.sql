561b04095eda6560e66559467852f8e8c35c2c6f
clieftink <clieftink@80c535d0-b110-0410-b4de-9279f94142c3>
2009-09-01 10:55:12 +0000

    Added to the sql-script for adding column owner, an insert statement in schema_history.
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@3448 80c535d0-b110-0410-b4de-9279f94142c3

--- database/manual_schema_updates/3243.sql
+++ database/manual_schema_updates/3243.sql
@@ -1,7 +1,17 @@
+BEGIN;
+
 alter table library
-add column owner_screener_id integer
+add column owner_screener_id integer;
 
 alter table library add
   CONSTRAINT fk_library_to_owner FOREIGN KEY (owner_screener_id)
       REFERENCES screening_room_user (screensaver_user_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION;
+ 
+INSERT INTO schema_history (screensaver_revision, date_updated, comment)
+SELECT
+3243,
+current_timestamp,
+'Added column owner to table library.';     
+      
+COMMIT;
