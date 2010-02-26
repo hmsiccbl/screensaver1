a1ad1b64c2dd51d5ae6551516425fe8826f197bf
atolopko <atolopko@80c535d0-b110-0410-b4de-9279f94142c3>
2010-02-26 16:11:29 +0000

    - Improvements to schema_updates_required.pl script, allowing "from revision" argument to be specified.  This allows the script to be used to report the required migration update scripts that are needed when upgrading from a previous version of Screensaver. - Added wrapper scripts for determining the set of required migration update scripts from versions v1.7.4 and v2009.09.16.
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@3839 80c535d0-b110-0410-b4de-9279f94142c3

--- database/manual_schema_updates/schema_updates_required.pl
+++ database/manual_schema_updates/schema_updates_required.pl
@@ -1,41 +1,42 @@
 #!/usr/bin/perl -w
 
-# Find the schema updates scripts that need to be applied to the
-# current database by comparing the updates already applied, as
-# recorded in the schema_history database table, to the full set of
-# update scripts in the database/manual_schema_updates directory.
+# Find the schema migration updates scripts that need to be applied to the
+# specified database.  This set of required scripts is determined by 
+# comparing the updates already applied, as recorded in the schema_history 
+# database table, to the set of update scripts in the specified directory.
 
-# update-schema.pl <ss_dir> <db name> <db user> <db host>
+# Usage:
+# schema_updates_required.pl <from revision> <migration scripts dir> <db name> <db user> <db host>
 
-my $ss_dir = shift @ARGV;
+use File::Find;
+
+my $from_revision = shift @ARGV;
+my $migration_scripts_dir = shift @ARGV;
 my $db_name = shift @ARGV;
 my $db_user = shift @ARGV;
 my $db_host = shift @ARGV || "localhost";
-my $min_rev_to_consider = 1500;
-my $script_dir = "$ss_dir/database/manual_schema_updates";
 
 my $connect_options = "-h $db_host -d $db_name -U $db_user";
 my $psql_cmd = "psql $connect_options";
 print STDERR "psql cmd: $psql_cmd\n";
 
-my %revs_applied = ();
-for $line (split(/\n/, `ls -1 $script_dir/*sql`)) {
-  if ($line =~ /([0-9]+)/) {
-    $revs_applied{$1} = 0;
-  }
-}
-#print(join("\n", sort keys %revs_applied));
+my %revisions = ();
+
+sub process { if (/^([0-9]+)\.sql$/) { if ($1 >= $from_revision) { $revisions{$1} = 0 } } }
+find({ wanted => \&process,  }, $migration_scripts_dir);
+#print(join("\n", sort keys %revisions));
 
 push @output, split('\n', `$psql_cmd --tuples-only -c 'select screensaver_revision from schema_history'`);
 for $line (@output) {
   $line =~ /^\s*([0-9]+)/;
-  warn "unknown revision $1 was apparently applied\n" unless exists $revs_applied{$1};
-  $revs_applied{$1} = 1 if $1;
+  $rev_applied = $1;
+ if ($rev_applied >= $from_revision and !exists $revisions{$rev_applied}) {
+   warn "migration script for revision $rev_applied was apparently applied, but migration script does not exist\n";
+ }
+  $revisions{$1} = 1 if $1;
 }
 
-print "schema migrations needed (>= rev $min_rev_to_consider):\n";
-for $rev (sort keys %revs_applied) {
-  unless ($revs_applied{$rev} || $rev < $min_rev_to_consider) {
-    print "$rev\n";
-  }
+print STDERR "schema migrations required (>= rev $from_revision):\n";
+for $rev (sort keys %revisions) {
+  print "$rev\n"  unless $revisions{$rev};
 }
---
+++ database/manual_schema_updates/schema_updates_required_since_v2009.09.16.sh
@@ -0,0 +1,4 @@
+# Usage:
+# schema_updates_required_since_v2009.09.16.sh <migration scripts dir> <db name> <db user> <db host>
+
+perl schema_updates_required.pl 3471 "$@"
