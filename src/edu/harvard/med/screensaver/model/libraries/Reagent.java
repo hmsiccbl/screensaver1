19f01f8bd4a6f0e40fd07994938c2ffd6ac90cba
atolopko <atolopko@80c535d0-b110-0410-b4de-9279f94142c3>
2010-04-07 19:02:39 +0000

    clean imports
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@3957 80c535d0-b110-0410-b4de-9279f94142c3

--- src/edu/harvard/med/screensaver/model/libraries/Reagent.java
+++ src/edu/harvard/med/screensaver/model/libraries/Reagent.java
@@ -29,7 +29,6 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 import javax.persistence.UniqueConstraint;
 
 import edu.harvard.med.screensaver.model.AbstractEntity;
