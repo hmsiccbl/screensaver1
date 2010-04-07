f676eb463faed634c14f005bb240f1e952e10467
atolopko <atolopko@80c535d0-b110-0410-b4de-9279f94142c3>
2010-04-07 19:02:22 +0000

    javadoc update
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@3956 80c535d0-b110-0410-b4de-9279f94142c3

--- src/edu/harvard/med/screensaver/ui/package-info.java
+++ src/edu/harvard/med/screensaver/ui/package-info.java
@@ -1,10 +1,9 @@
 /**
- * Provides interfaces/classes that implement the user interface for
- * the Screensaver web application. Implemented within the Java
- * ServerFaces (JSF) framework. Subpackages primarily contain JSF
- * {@link AbstractBackingBean backing bean} classes, which comprise
- * the state management and behavioral methods for various the JSF
- * views (web pages).  Backing bean classes delegate navigation logic
- * and business logic to the controller classes.
+ * Provides interfaces/classes that implement the user interface for the
+ * Screensaver web application. Implemented with the JavaServer Faces (JSF)
+ * framework. Subpackages primarily contain JSF {@link AbstractBackingBean
+ * backing bean} classes, which comprise the state management and behavioral
+ * methods for the various JSF views (web pages). Backing bean classes delegate
+ * navigation logic and business logic to the controller classes.
  */
 package edu.harvard.med.screensaver.ui;
