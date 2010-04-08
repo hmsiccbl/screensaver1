344bb660a4cd9e8dc37a5c6ac126cbc369853bd3
atolopko <atolopko@80c535d0-b110-0410-b4de-9279f94142c3>
2010-04-08 17:12:03 +0000

    added scripts to update files' svn keywords and copyright notices
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@3969 80c535d0-b110-0410-b4de-9279f94142c3

---
+++ bin/dev/update-copyright.sh
@@ -0,0 +1,3 @@
+# appends a new year to the copyright notice in every file below the current directory that currently contains a copyright notice
+# TODO: update this file as necessary with the previous and current year before running
+find . -type f -not -path '*/.svn/*' -exec grep -l 'Copyright © ' {} \; | xargs -i perl -pi -e 's/(Copyright © .*, 2010)/$1, 2011/'  {}
