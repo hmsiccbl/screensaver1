ced3adda3a3b97e99237b8d00f5334be6a4b444a
atolopko <atolopko@80c535d0-b110-0410-b4de-9279f94142c3>
2010-04-14 16:26:15 +0000

    fixed index name in 3907.sql
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@4017 80c535d0-b110-0410-b4de-9279f94142c3

--- database/manual_schema_updates/3907.sql
+++ database/manual_schema_updates/3907.sql
@@ -6,7 +6,8 @@ SELECT
 current_timestamp,
 'result_value.value set to null for numeric data columns, since it is never used';
 
-drop index result_value_rvt_and_value_index;
+drop index result_value_data_column_and_value_index;
 update result_value set value = null from data_column dc where result_value.data_column_id = dc.data_column_id and dc.data_type = 'Numeric';
-create index result_value_rvt_and_value_index on result_value (result_value_type_id, value);
+create index result_value_data_column_and_value_index on result_value (data_column_id, value);
+
 COMMIT;
