c5cc263e53815979ccaeea6730c461a3bc5082c4
atolopko <atolopko@80c535d0-b110-0410-b4de-9279f94142c3>
2010-04-14 15:35:53 +0000

    Resolved [#2329] (3873.sql schema migration script does not create new boolean positives data column)
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@4011 80c535d0-b110-0410-b4de-9279f94142c3

--- database/manual_schema_updates/3873.sql
+++ database/manual_schema_updates/3873.sql
@@ -6,8 +6,16 @@ SELECT
 current_timestamp,
 'remove "Numerical" positive indicator type from result_value_type';
 
-update result_value_type set how_derived = (case when how_derived is null then '' else how_derived || ' ' end) || '[positives determined using cutoff value of ' || positive_indicator_cutoff || ' where ' || positive_indicator_direction || ' a positive]' where positive_indicator_type = 'Numerical';
-update result_value_type set positive_indicator_type = 'Boolean' where positive_indicator_type = 'Numerical';
+/* TODO: this script converts Numerical Positive Indicator result
+value types (aka data headers/columns) to a basic numeric data type.
+At ICCB-L all screen results that used Numerical Positive Indicator
+also had additional positives data columns that were derived from
+these columns, and so positives determinations were not lost.  If your
+database has screen results that relied exclusvely on Numerical
+Positive Indicator data columns for positives determinations, then you
+should create a new positives data column to record the positives.  */
+
+update result_value_type set is_positive_indicator = false, is_numeric = true, positive_indicator_type = null where positive_indicator_type = 'Numerical';
 
 alter table result_value_type drop column positive_indicator_cutoff;
 alter table result_value_type drop column positive_indicator_direction;
