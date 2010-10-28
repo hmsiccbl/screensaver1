7a3ba77274f84d81e11adca648bb6473990f7cb8
atolopko <atolopko@80c535d0-b110-0410-b4de-9279f94142c3>
2010-10-28 20:10:38 +0000

    updated initialize-example-users.sql script
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@4889 80c535d0-b110-0410-b4de-9279f94142c3

--- database/example-database/initialize_example_users.sql
+++ database/example-database/initialize_example_users.sql
@@ -15,9 +15,8 @@ begin;
 insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id, digested_password) values (nextval('screensaver_user_id_seq'), 1, 'today', 'Screensaver', 'Guest', '', 'guest', '35675e68f4b5af7b995d9205ad0fc43842f16450');
 insert into screening_room_user (screensaver_user_id, user_classification) values (currval('screensaver_user_id_seq'), 'Other');
 insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screensaverUser';
-insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screeningRoomUser';
-insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'compoundScreeningRoomUser';
-insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'rnaiScreeningRoomUser';
+insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'smDsl1MutualScreens';
+insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'rnaiScreens';
 
 /* Create a 'dev' administrator account. */
 insert into screensaver_user (screensaver_user_id, version, date_created, first_name, last_name, email, login_id, digested_password) values (nextval('screensaver_user_id_seq'), 1, 'today', 'Screensaver', 'Developer', '', 'dev', '34c6fceca75e456f25e7e99531e2425c6c1de443');
@@ -27,12 +26,14 @@ insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) s
 insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'readEverythingAdmin';
 insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'userChecklistItemsAdmin';
 insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'usersAdmin';
+insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'userRolesAdmin';
 insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'labHeadsAdmin';
 insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'librariesAdmin';
+insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'libraryCopiesAdmin';
 insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screensAdmin';
 insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'billingAdmin';
 insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screenResultsAdmin';
-insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'dataSharingLevelAdmin';
+insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'screenDataSharingLevelsAdmin';
 insert into screensaver_user_role (screensaver_user_id, screensaver_user_role) select currval('screensaver_user_id_seq'), 'cherryPickRequestsAdmin';
 
 
