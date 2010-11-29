2f9f872a152143e5babdabcfe56faff743be2e26
atolopko <atolopko@80c535d0-b110-0410-b4de-9279f94142c3>
2010-11-29 16:00:09 +0000

    [#2696] update of SQL report to provide usage statistics for library screening plates
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@5071 80c535d0-b110-0410-b4de-9279f94142c3

--- database/reports/library-screening-plates-usage.sql
+++ database/reports/library-screening-plates-usage.sql
@@ -8,16 +8,16 @@ make this data accessible via the "Well Volumes Browser" in the web
 application. */
 
 select l.short_name as library, 
-pu.copy_name, 
+c.name, 
 p.plate_number as plate_number, 
 count(*) as usage_count, 
-ci.well_volume as initial_volume_nL, 
-sum(volume_transferred_per_well * number_of_replicates) as volume_used_nL, 
-ci.well_volume - sum(volume_transferred_per_well * number_of_replicates) as volume_remaining_nL  
-from plates_used pu 
-join (select distinct plate_number from well) as p on(p.plate_number between pu.start_plate and pu.end_plate) 
-join library l on(p.plate_number between l.start_plate and l.end_plate) 
-join lab_activity la on(la.activity_id = pu.library_screening_id) 
-join screening using(activity_id) 
-left join copy_info ci on(ci.copy_id = l.short_name || ':' || pu.copy_name and ci.plate_number=p.plate_number)
-group by l.short_name, pu.copy_name, p.plate_number, ci.well_volume;
+p.well_volume as initial_volume_nL, 
+sum(la.volume_transferred_per_well * s.number_of_replicates) as volume_used_nL, 
+p.well_volume - sum(la.volume_transferred_per_well * s.number_of_replicates) as volume_remaining_nL  
+from library l 
+join copy c using(library_id)
+join plate p using(copy_id)
+join assay_plate ap using(plate_id)
+join lab_activity la on(ap.library_screening_id = la.activity_id)
+join screening s using(activity_id)
+group by l.short_name, c.name, p.plate_number, p.well_volume;
