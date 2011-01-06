9ce4c0236ed2ced271b471fb5f94d55349d90cce
atolopko <atolopko@80c535d0-b110-0410-b4de-9279f94142c3>
2011-01-06 18:11:19 +0000

    command-line arg fixes to import_screen_result.sh; updated usage comments
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@5161 80c535d0-b110-0410-b4de-9279f94142c3

--- bin/io/import_screen_result.sh
+++ bin/io/import_screen_result.sh
@@ -1,14 +1,13 @@
-# first, run 'ant -Dscreensaver.properties.file=<file> -Dinstall.dir install'
+# ICCB-L-specific script for running the cmd-line ScreenResultImporter 
+# 
+# To install:
+# $ ant -Dscreensaver.properties.file=<file> -Dinstall.dir=<dir> install
+# Configure database connection arguments in $SCREENSAVER/classes/screensaver.properties
 #
-# usage:
-# bsub $SCREENSAVER/bin/io/import_screen_result.sh <screen#> [<start plate #> <end plate #> [<'append'>]]
-#
-# usage (interactive/debug):
-# bsub -Is -q shared_int_2h $SCREENSAVER/bin/io/import_screen_result.sh <screen#> [<start plate #> <end plate #> [<'append'>]]
+# To run:
+# bsub $SCREENSAVER/bin/io/import_screen_result.sh <screen#> <admin eCommons ID> [<start plate #> <end plate #> [<'append'>]]
 #
 # Screen result file must be $SCREEN_RESULTS/<screen#>_finalResults.xls
-#
-# Configure database connection arguments in $SCREENSAVER/classes/screensaver.properties
 
 SCREENSAVER=`dirname $0`/../..
 cd $SCREENSAVER
@@ -18,6 +17,7 @@ LIBS=`for s in $SCREENSAVER/lib/*.jar ; do printf ":$s" ; done`
 CLASSPATH="$SCREENSAVER/classes$LIBS"
 
 SCREEN_NUMBER=$1
+ADMIN_ECOMMONS_ID=$2
 SCREEN_RESULTS=${SCREEN_RESULTS:-$HOME/screen-results}
 START_PLATE=$2
 END_PLATE=$3
@@ -25,7 +25,8 @@ APPEND=$4
 
 java -Xmx1500m -cp $CLASSPATH \
     edu.harvard.med.screensaver.io.screenresults.ScreenResultImporter \
-    -f $SCREEN_RESULTS/${SCREEN_NUMBER}_finalResults.xls -s ${SCREEN_NUMBER} -w 4 -i \
+    -AE $2
+    -f $SCREEN_RESULTS/${SCREEN_NUMBER}_finalResults.xls -s ${SCREEN_NUMBER} -i \
     ${START_PLATE:+ -sp $START_PLATE} ${END_PLATE:+ -ep $END_PLATE} ${APPEND:+ --append} \
     > import_screen_result.out \
     2> import_screen_result.err
