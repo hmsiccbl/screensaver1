57b5e4ad29700c1dcd46e0b5a883eeecbed9279e
atolopko <atolopko@80c535d0-b110-0410-b4de-9279f94142c3>
2011-04-27 19:58:20 +0000

    run.sh now accepts a '-d' cmd line flag for supporting remote debugging session; this is required for debugging the cmd line apps as generated by a maven assembly (cmd line apps are no longer easily runnable/debugabble from Eclipse as Java applications)
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/branches/iccbl/maven-multimodule@5720 80c535d0-b110-0410-b4de-9279f94142c3

--- batch/src/main/scripts/run.sh
+++ batch/src/main/scripts/run.sh
@@ -1,13 +1,17 @@
 # runs a Screensaver command-line utility
 # usage: run.sh <fully qualifed class> <arg>...
 
+while getopts 'd' opt; do
+  echo Running in debug mode...
+  DEBUG_OPTIONS=-agentlib:jdwp=transport=dt_socket,server=y,address=8000
+done
+shift $(( $OPTIND - 1 ))
+
 SCREENSAVER=.
 JAVA=$JAVA_HOME/bin/java 
-MAX_RAM_MB=800
+MAX_RAM=800m
 RESOURCES=$SCREENSAVER/resources
-JARS=`find $SCREENSAVER -name "*.jar" -print`
+JARS=`find $SCREENSAVER/lib -name "*.jar" -print`
 LIBS=`for s in $JARS ; do printf ":$s" ; done`
 CLASSPATH=.:$RESOURCES:$LIBS
-$JAVA -Xmx${MAX_RAM_MB}m -cp $CLASSPATH "$@"
-
-
+$JAVA -Xmx${MAX_RAM} $DEBUG_OPTIONS -cp $CLASSPATH "$@"
