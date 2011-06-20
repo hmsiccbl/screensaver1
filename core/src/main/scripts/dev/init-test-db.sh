1a10f63a2e93abcf31c978ee919ebdc128e2a4cc
atolopko <atolopko@80c535d0-b110-0410-b4de-9279f94142c3>
2011-06-20 01:19:53 +0000

    - updated pom versions to 6019 - fixed init-test.db.sh for maven
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@6020 80c535d0-b110-0410-b4de-9279f94142c3

--- core/src/main/scripts/dev/init-test-db.sh
+++ core/src/main/scripts/dev/init-test-db.sh
@@ -1,8 +1,8 @@
 DB_NAME=$1
 DB_USER=$2
-PRJ_DIR=`dirname $0`/../..
-CREATE_SCHEMA_SQL=$PRJ_DIR/build/ddl/create_schema.sql
+PRJ_DIR=`dirname $0`/../../../..
+CREATE_SCHEMA_SQL=$PRJ_DIR/target/hibernate3/sql/screensaver_schema.sql
 dropdb $DB_NAME -U $DB_USER
 createdb $DB_NAME -U $DB_USER
-(cd $PRJ_DIR && ant ddl -Dscreensaver.properties.file=cfg/screensaver.properties.test)
+(cd $PRJ_DIR && mvn org.codehaus.mojo:hibernate3-maven-plugin:2.2:hbm2ddl)
 psql $DB_NAME $DB_USER -f $CREATE_SCHEMA_SQL
