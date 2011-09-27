58fcb5f246bf59d8ee56699977ec4b936d608ad1
seanderickson1 <seanderickson1@80c535d0-b110-0410-b4de-9279f94142c3>
2011-09-27 17:47:03 +0000

    [#2920] add Well concentration field: - add precondition statements to the migration script so that it can be evaluated for use in other facilities - cause the script to fail if concentration values for the plates in a copy are not all the same (a precondition).
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@6486 80c535d0-b110-0410-b4de-9279f94142c3

--- core/src/main/sql/schema_updates/6359.sql
+++ core/src/main/sql/schema_updates/6359.sql
@@ -8,23 +8,28 @@ current_timestamp,
 
 /** TODO: this is an ICCB-L specific data migration script to be performed after schema updates are applied for the ticket:
  * [#2920] - store concentration values at the well level
- * This script migrates concentration values from the plate to the new values on the well and the copy entity.
+ * This script migrates concentration values from the plate to the new values on the well, it creates derived aggregate (min/max/primary) concentration
+ * values on the plate and the copy entity, and it calculates a dilution factor that will be stored on the copy.
+ * 
+ * PRECONDITION 1: All concentration values for the plates within a copy are the same value.  (the new schema enforces this).
+ * This script will fail if all concentration values for the plates within a copy are the same value.  Use this sql to determine:
+ * select * from (select copy_id, count(distinct mg_ml_concentration) from plate join copy using(copy_id) where mg_ml_concentration is not null group by copy_id) as a where count > 1;
+ * and,
+ * select * from (select copy_id, count(distinct molar_concentration) from plate join copy using(copy_id) where molar_concentration is not null group by copy_id) as a where count > 1;
+ * 
+ * PRECONDITION 2: Current plates have either a mg_ml_concentration value, or a molar_concentration value, but not both.
+ * This script will not fail if this condition is not met, however, it will arbitrarily use the molar_concentration value in conflicting records (due to ordering).
+ * use this sql to verify:
+ * select plate_number, c.name, short_name, mg_ml_concentration, molar_concentration from plate p join copy c using(copy_id) join library l using(library_id) where mg_ml_concentration is not null and molar_concentration is not null;
+ * 
+ * Well concentrations are set to the maximum concentration found for all plates in a library, 
+ * the copy dilution factor is determined by the ratio (max concentration in the library / concentration of the copy plates ).
  */
 
-/** all natural product libraries should be initialized to 15 mg/ml per CES, Informatics 2011-09-08 **/
-update plate set mg_ml_concentration = 15.0 
-  where mg_ml_concentration is null 
-  and plate_number in (select plate_number from plate join copy using(copy_id) join library using (library_id) where library_type like 'Natural Products');
-
-/** this fixes an extant error in the data, discussed with Doug on 9/23; there are 12 library plates with _both_ mg/ml and molar concentration values.  In all of these cases, the molar concentration will be used. - sde4 **/
-update plate set mg_ml_concentration = null where plate_id in 
-  (select plate_id from plate join copy using(copy_id) join library using(library_id) 
-    where screen_type = 'Small Molecule' 
-    and molar_concentration is not null and mg_ml_concentration is not null);
-  
 /** perform the migration **/
     
-update copy c set primary_well_mg_ml_concentration = (select max(mg_ml_concentration) from plate join copy c1 using(copy_id) where c1.copy_id = c.copy_id );  
+/** THIS COMMAND WILL FAIL IF PRECONDITION 1 LISTED ABOVE IS NOT MET **/
+update copy c set primary_well_mg_ml_concentration = (select distinct(mg_ml_concentration) from plate join copy c1 using(copy_id) where c1.copy_id = c.copy_id and plate.mg_ml_concentration is not null );
 update copy c set well_concentration_dilution_factor =  
   (select max(primary_well_mg_ml_concentration) from copy c1 where c1.library_id = c.library_id) / primary_well_mg_ml_concentration where primary_well_mg_ml_concentration is not null;
 update copy c set primary_well_mg_ml_concentration = (select max(primary_well_mg_ml_concentration) from copy c1 where c1.library_id = c.library_id);  
@@ -32,7 +37,8 @@ update copy set max_mg_ml_concentration = primary_well_mg_ml_concentration;
 update copy set min_mg_ml_concentration = primary_well_mg_ml_concentration;
 
   
-update copy c set primary_well_molar_concentration = (select max(molar_concentration) from plate join copy c1 using(copy_id) where c1.copy_id = c.copy_id );  
+/** THIS COMMAND WILL FAIL IF PRECONDITION 1 LISTED ABOVE IS NOT MET **/
+update copy c set primary_well_molar_concentration = (select distinct(molar_concentration) from plate join copy c1 using(copy_id) where c1.copy_id = c.copy_id and plate.molar_concentration is not null );
 update copy c set well_concentration_dilution_factor =  
   (select max(primary_well_molar_concentration) from copy c1 where c1.library_id = c.library_id) / primary_well_molar_concentration where primary_well_molar_concentration is not null;
 update copy c set primary_well_molar_concentration = (select max(primary_well_molar_concentration) from copy c1 where c1.library_id = c.library_id);  
