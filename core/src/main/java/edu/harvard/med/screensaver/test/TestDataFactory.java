01a501384d850333b7f3e8521a1396f31477332d
seanderickson1 <seanderickson1@80c535d0-b110-0410-b4de-9279f94142c3>
2012-01-11 15:24:12 +0000

    comments
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@6923 80c535d0-b110-0410-b4de-9279f94142c3

--- core/src/main/java/edu/harvard/med/screensaver/test/TestDataFactory.java
+++ core/src/main/java/edu/harvard/med/screensaver/test/TestDataFactory.java
@@ -536,12 +536,12 @@ public class TestDataFactory
         BigDecimal val = null;
         // TODO: we should be able to generalize these special case BigDecimal builders by inspecting the model annotations for precision and scale and then construct a BigDecimal that is appropriate for the test
         if (callStack.contains("MlConcentration")) {
-          // mgMlConcentration has a max precision 5, scale 3
+          // mgMlConcentration has a max precision 3, scale 5
           val = new BigDecimal((TestDataFactory.this.newInstance(Double.class)).doubleValue() / _doubleTestValue);
           val = val.setScale(3, RoundingMode.HALF_UP);
         }
         else if (callStack.contains("dilution")) {
-          // dilution factor has a max precision 2, scale 8
+          // dilution factor has a max precision 8, scale 2
           val = new BigDecimal((TestDataFactory.this.newInstance(Double.class)).doubleValue() / (_doubleTestValue / 10));
           val = val.setScale(2, RoundingMode.HALF_UP);
         }
