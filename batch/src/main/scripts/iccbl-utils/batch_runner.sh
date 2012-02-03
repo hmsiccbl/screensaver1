dddf53331bca7ecc6e31b6c35b43d335dca28629
seanderickson1 <seanderickson1@80c535d0-b110-0410-b4de-9279f94142c3>
2012-02-03 15:57:33 +0000

    update the directory for the utils job runners
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@7014 80c535d0-b110-0410-b4de-9279f94142c3

---
+++ batch/src/main/scripts/iccbl-utils/batch_runner.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# runs the batch jobs for the Screensaver ICCBL databases
+
+DIR=.
+
+export JAVA_HOME=/usr/lib/jvm/java-6-sun/
+
+if [[ $# -lt 1 ]]
+then
+  echo "Usage: $0 [local || dev || stage || prod ] "
+  exit $WRONG_ARGS
+fi
+
+SERVER=$1
+shift 1
+
+if [[ "$SERVER" == "PROD" ]] || [[ "$SERVER" == "prod" ]]
+then
+  export SCREENSAVER_PROPERTIES_FILE=/groups/screensaver/screensaver-configs/screensaver.properties.batch.prod
+elif [[ "$SERVER" == "STAGE" ]] || [[ "$SERVER" == "stage" ]]
+then
+  export SCREENSAVER_PROPERTIES_FILE=/groups/screensaver/screensaver-configs/screensaver.properties.batch.stage
+elif [[ "$SERVER" == "DEV" ]] || [[ "$SERVER" == "dev" ]]
+then
+  export SCREENSAVER_PROPERTIES_FILE=/groups/screensaver/screensaver-configs/screensaver.properties.batch.dev
+else
+  echo "Unknown option: \"$SERVER\""
+  exit 1
+fi
+
+cd $DIR
+$DIR/bjobs.sh bsub -u sean.erickson.hms@gmail.com -q shared_12h $DIR/run.sh "$@"
