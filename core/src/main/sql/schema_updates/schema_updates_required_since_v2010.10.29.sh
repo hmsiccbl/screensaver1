7220480d66bd93215c51b4d5557d8551f7adfd31
seanderickson1 <seanderickson1@80c535d0-b110-0410-b4de-9279f94142c3>
2012-02-06 23:36:40 +0000

    account for updates that were created for the LINCS branch (4406-4663) and then merged after v2010.10.29
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@7019 80c535d0-b110-0410-b4de-9279f94142c3

--- core/src/main/sql/schema_updates/schema_updates_required_since_v2010.10.29.sh
+++ core/src/main/sql/schema_updates/schema_updates_required_since_v2010.10.29.sh
@@ -1,4 +1,4 @@
 # Usage:
 # schema_updates_required_since_v2010.10.29.sh <db name> <db user> <db host>
 
-cd `dirname $0` && perl schema_updates_required.pl --from-revision 4406 --scripts-dir . --db-name $1 --db-user $2 ${3:+--db-host $3}
+cd `dirname $0` && perl schema_updates_required.pl --from-revision 4898 --revision 4406 --revision 4447 --revision 4448 --revision 4449 --revision 4576 --revision 4663 --scripts-dir . --db-name $1 --db-user $2 ${3:+--db-host $3}
