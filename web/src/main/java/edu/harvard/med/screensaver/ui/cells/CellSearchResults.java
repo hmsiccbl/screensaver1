89d8d76eff38d1b975a7203d0edc3fc906ddaf9d
seanderickson1 <seanderickson1@80c535d0-b110-0410-b4de-9279f94142c3>
2012-04-05 15:03:59 +0000

    update text
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@7129 80c535d0-b110-0410-b4de-9279f94142c3

--- web/src/main/java/edu/harvard/med/screensaver/ui/cells/CellSearchResults.java
+++ web/src/main/java/edu/harvard/med/screensaver/ui/cells/CellSearchResults.java
@@ -88,9 +88,10 @@ public class CellSearchResults extends EntityBasedEntitySearchResults<Cell, Inte
 				return true;
 			}
 		});
+		// CL:1
 		columns.get(columns.size() - 1).setVisible(true);
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("name"), "Cell Name",
-				"CL:1 - The primary name for the cell line as chosen by LINCS", TableColumn.UNGROUPED) {
+				"The primary name for the cell line as chosen by LINCS", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getName();
@@ -98,44 +99,49 @@ public class CellSearchResults extends EntityBasedEntitySearchResults<Cell, Inte
  		});
 		columns.get(columns.size() - 1).setVisible(true);
 
+		// CL:2 
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("cloId"), "CL ID",
-				"CL:2 - Unique LINCS internal identifier", TableColumn.UNGROUPED) {
+				"Unique LINCS internal identifier", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getCloId();
 			}
 		});
 		columns.get(columns.size() - 1).setVisible(true);
-
+		
+		// CL:3
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("alternateName"), "Alternate Name",
-				"CL:3 - Other relevant names", TableColumn.UNGROUPED) {
+				"Other relevant names", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getAlternateName();
 			}
 		});
 		columns.get(columns.size() - 1).setVisible(false);
-
+		
+		// CL:4
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("alternateId"), "Alternate ID",
-				"CL:4 - Other relevant IDs for cell lines", TableColumn.UNGROUPED) {
+				"Other relevant IDs for cell lines", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getAlternateId();
 			}
 		});
 		columns.get(columns.size() - 1).setVisible(false);
-
+		
+		// CL:5
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("centerName"), "Center Name",
-				"CL:5 - LINCS center using the cell line", TableColumn.UNGROUPED) {
+				"LINCS center using the cell line", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getCenterName();
 			}
 		});
 		columns.get(columns.size() - 1).setVisible(true);
-
+		
+		// CL:6
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("centerSpecificId"), "Center Specific ID",
-				"CL:6 - HMS or Broad cell line ID; batch specific ID", TableColumn.UNGROUPED) {
+				"HMS or Broad cell line ID; batch specific ID", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getCenterSpecificId();
@@ -143,80 +149,98 @@ public class CellSearchResults extends EntityBasedEntitySearchResults<Cell, Inte
 		});
 		columns.get(columns.size() - 1).setVisible(false); // We're hiding this field, and showing the HMS facility ID, which is the same thing, for our facility
 
+		// CL:7
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("vendorName"), "Cell Vendor/Provider",
-				"CL:7 - Name of vendor or lab(provider)  that supplied the cell line", TableColumn.UNGROUPED) {
+				"Name of vendor or lab(provider)  that supplied the cell line", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getVendor();
 			}
 		});
 		columns.get(columns.size() - 1).setVisible(true);
+		
+		// CL:8
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("vendorCatalogId"), "Cell Vendor/Provider Catalog ID",
-				"CL:8 - ID or catalogue number or name assigned to the cell line by the vendor or provider", TableColumn.UNGROUPED) {
+				"ID or catalogue number or name assigned to the cell line by the vendor or provider", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getVendorCatalogId();
 			}
 		});
 		columns.get(columns.size() - 1).setVisible(true);
+		
+		// CL:9
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("batchId"), "Cell Vendor/Provider Batch ID",
-				"CL:9 - Vendor/Provider Batch ID number; Batch or lot number assigned to the cell line by the vendor or provider", TableColumn.UNGROUPED) {
+				"Vendor/Provider Batch ID number; Batch or lot number assigned to the cell line by the vendor or provider", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getBatchId();
 			}
 		});
 		columns.get(columns.size() - 1).setVisible(true);
+		
+		// CL:10 
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("organism"), "Organism",
-				"CL:10 - Organism of origin; a controlled vocabulary describing the organism from which the cell line was derived (e.g. Homo sapiens, Mus musculus, etc.)", TableColumn.UNGROUPED) {
+				"Organism of origin; a controlled vocabulary describing the organism from which the cell line was derived (e.g. Homo sapiens, Mus musculus, etc.)", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getOrganism();
 			}
 		});
+		
+		//CL:11 
 		columns.get(columns.size() - 1).setVisible(true);
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("organ"), "Organ",
-				"CL:11 - Organ of origin; controlled terms describing the organ from which cell line is derived;  (e.g. lung, mammary gland etc.)", TableColumn.UNGROUPED) {
+				"Organ of origin; controlled terms describing the organ from which cell line is derived;  (e.g. lung, mammary gland etc.)", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getOrgan();
 			}
 		});
 		columns.get(columns.size() - 1).setVisible(true);
+		
+		// CL:12
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("tissue"), "Tissue",
-				"CL:12 - Tissue of origin; A controlled vocabulary describing the tissue from which the cell line was derived", TableColumn.UNGROUPED) {
+				"Tissue of origin; A controlled vocabulary describing the tissue from which the cell line was derived", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getTissue();
 			}
 		});
+		
+		// CL:13
 		columns.get(columns.size() - 1).setVisible(true);
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("cellType"), "Cell Type",
-				"CL:13 - A controlled vocabulary describing  the cell type from which a cell line was derived; e.g. epithelial like, fibroblast-like, lymphoblast like, hematopoetic, mesenchymal, neural, etc. This provides information about cell morphology.  Also sometimes referred to as cell morphology.", TableColumn.UNGROUPED) {
+				"A controlled vocabulary describing  the cell type from which a cell line was derived; e.g. epithelial like, fibroblast-like, lymphoblast like, hematopoetic, mesenchymal, neural, etc. This provides information about cell morphology.  Also sometimes referred to as cell morphology.", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getCellType();
 			}
 		});
 		columns.get(columns.size() - 1).setVisible(true);
+		
+		// CL:14
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("disease"), "Disease",
-				"CL:14 - If the cell line came from a particular diseased tissue, the disease should be noted in terms of a controlled vocabulary (e.g. breast cancer, colon cancer, not diseased, etc.)", TableColumn.UNGROUPED) {
+				"If the cell line came from a particular diseased tissue, the disease should be noted in terms of a controlled vocabulary (e.g. breast cancer, colon cancer, not diseased, etc.)", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getDisease();
 			}
 		});
 		columns.get(columns.size() - 1).setVisible(true);
+		
+		// CL:15
 		columns.add(new TextSetEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("growthProperties"), "Growth Properties",
-				"CL:15 - A controlled vocabulary describing the growth properties of the cell line (e.g. adherent, suspension)", TableColumn.UNGROUPED) {
+				"A controlled vocabulary describing the growth properties of the cell line (e.g. adherent, suspension)", TableColumn.UNGROUPED) {
 			@Override
 			public Set<String> getCellValue(Cell info) {
 				return info.getGrowthProperties();
 			}
 		});
 
+		// CL:16
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("geneticModificaton"), "Genetic Modification",
-				"CL:16 - Stable transfection or viral transduction.  If yes, the modifications (e.g. expressing GFP-tagged protein) should be described and appropriate references provided. ", TableColumn.UNGROUPED) {
+				"Stable transfection or viral transduction.  If yes, the modifications (e.g. expressing GFP-tagged protein) should be described and appropriate references provided. ", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getGeneticModification();
@@ -224,8 +248,9 @@ public class CellSearchResults extends EntityBasedEntitySearchResults<Cell, Inte
 		});
 		columns.get(columns.size() - 1).setVisible(true);
 
+		// CL:17
 		columns.add(new TextSetEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("relatedProjects"), "Related Projects",
-				"CL:17 - Other projects in which the cell line has been studied / used; A controlled vocabulary describing other large scale projects in which the cell line has been used (e.g. ENCODE, TCGA, ICBP, Epigenomics, etc.)", TableColumn.UNGROUPED) {
+				"Other projects in which the cell line has been studied / used; A controlled vocabulary describing other large scale projects in which the cell line has been used (e.g. ENCODE, TCGA, ICBP, Epigenomics, etc.)", TableColumn.UNGROUPED) {
 			@Override
 			public Set<String> getCellValue(Cell info) {
 				return info.getRelatedProjects();
@@ -233,8 +258,9 @@ public class CellSearchResults extends EntityBasedEntitySearchResults<Cell, Inte
 		});
 		columns.get(columns.size() - 1).setVisible(false);
 
+		// CL:18
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("recommendedCultureConditions"), "Recommended Culture Conditions",
-				"CL:18 - A description of the standard tissue culture conditions (media, supplements, culture dish treatment) used to maintain the cell line.  Description of culture dish treatment conditions would include information about coating of culture dish with fibronectin, collagen, etc, prior to cell plating. If special culture vessels are required to grow the cells, these should also be mentioned and details provided.", TableColumn.UNGROUPED) {
+				"A description of the standard tissue culture conditions (media, supplements, culture dish treatment) used to maintain the cell line.  Description of culture dish treatment conditions would include information about coating of culture dish with fibronectin, collagen, etc, prior to cell plating. If special culture vessels are required to grow the cells, these should also be mentioned and details provided.", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getRecommendedCultureConditions();
@@ -242,8 +268,9 @@ public class CellSearchResults extends EntityBasedEntitySearchResults<Cell, Inte
 		});
 		columns.get(columns.size() - 1).setVisible(false);
 
+		// CL:19
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("verification"), "Verification",
-				"CL:19 - Information pertaining to experimental verification of the cell line identity", TableColumn.UNGROUPED) {
+				"Information pertaining to experimental verification of the cell line identity", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getVerification();
@@ -251,8 +278,9 @@ public class CellSearchResults extends EntityBasedEntitySearchResults<Cell, Inte
 		});
 		columns.get(columns.size() - 1).setVisible(false);
 
+		// CL:20
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("mutations"), "Mutations",
-				"CL:20 - Mutations inherent in certain cell lines", TableColumn.UNGROUPED) {
+				"Mutations inherent in certain cell lines", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getMutations();
@@ -260,8 +288,9 @@ public class CellSearchResults extends EntityBasedEntitySearchResults<Cell, Inte
 		});
 		columns.get(columns.size() - 1).setVisible(false);
 
+		// CL:21
 		columns.add(new TextEntityColumn<Cell>(RelationshipPath.from(Cell.class).toProperty("organismGender"), "Organism Gender",
-				"CL:21 - Whether cell line was obtained from a male or female subject", TableColumn.UNGROUPED) {
+				"Whether cell line was obtained from a male or female subject", TableColumn.UNGROUPED) {
 			@Override
 			public String getCellValue(Cell info) {
 				return info.getOrganismGender();
