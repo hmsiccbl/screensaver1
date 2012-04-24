ada17ad3d398bf717d844043e991e8ed702eedc1
seanderickson1 <seanderickson1@80c535d0-b110-0410-b4de-9279f94142c3>
2012-04-24 21:12:03 +0000

    improve help msg
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@7156 80c535d0-b110-0410-b4de-9279f94142c3

--- core/src/main/sql/schema_updates/schema_updates_required_since_v1.7.4.sh
+++ core/src/main/sql/schema_updates/schema_updates_required_since_v1.7.4.sh
@@ -1,4 +1,13 @@
 # Usage:
 # schema_updates_required_since_v1.7.4.sh <db name> <db user> <db host>
+expectedNumberArgs=2
+
+if [ $# -lt $expectedNumberArgs ]
+then
+  echo "Usage: `basename $0` $expectedNumberArgs args expected"
+  echo "schema_updates_required_since_v1.7.4.sh <db name> <db user> <db host>"
+  echo "args: $#"
+  exit $WRONG_ARGS
+fi
 
 cd `dirname $0` && perl schema_updates_required.pl --from-revision 2747 --scripts-dir . --db-name $1 --db-user $2 ${3:+--db-host $3}
