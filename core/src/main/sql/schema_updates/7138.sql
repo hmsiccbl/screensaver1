dbfa5784b672afc4f2979ef3a511f49770a652c5
seanderickson1 <seanderickson1@80c535d0-b110-0410-b4de-9279f94142c3>
2012-04-26 22:11:17 +0000

    update for [#3461] new Attached File types
    git-svn-id: https://forge.abcd.harvard.edu/svn/screensaver/trunk@7181 80c535d0-b110-0410-b4de-9279f94142c3

--- core/src/main/sql/schema_updates/7138.sql
+++ core/src/main/sql/schema_updates/7138.sql
@@ -6,6 +6,9 @@ SELECT
 current_timestamp,
 'add "Publication", "Other" attached_file_types';
 
+alter table attached_file_type drop constraint attached_file_type_value_key;
+alter table attached_file_type add constraint attached_file_type_key unique ( value, for_entity_type );
+
 /* TODO: ICCBL-specific update */
 insert into attached_file_type (attached_file_type_id, for_entity_type, value) values (nextval('attached_file_type_id_seq'), 'user', 'Publication');
 insert into attached_file_type (attached_file_type_id, for_entity_type, value) values (nextval('attached_file_type_id_seq'), 'user', 'Other');
